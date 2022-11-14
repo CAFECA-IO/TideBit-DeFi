4ed5e9f96210c617dcf07179d218f3a2a308531a
arealclimber <luffulgence@gmail.com>
2022-11-14 18:04:56 +0800

    [footer] trivial

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import TideButton from '../shared/button/tide_button';
-// import TideLink from '../shared/link/TideLink';
 import TideLink from '../shared/link/tide_link';
 
 const Footer = () => {
@@ -94,7 +93,7 @@ const Footer = () => {
   // {label: 'Terms of use', path: '/'}
 
   return (
-    <footer className="body-font lg:justify-center">
+    <footer className="body-font lg:justify-center -mb-10">
       <div className="container px-5 pt-24 pb-10 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
         <div className="lg:w-1/6 md:w-1/4 w-full px-4 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
           <a className="flex flex-col title-font font-medium items-center md:justify-start justify-center text-white">
@@ -125,64 +124,6 @@ const Footer = () => {
                 htmlref="noopener noreferrer"
               />
             ))}
-            {/* <TideLink
-							href="https://twitter.com/home"
-							content={
-								<svg
-									fill="currentColor"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									className="w-5 h-5"
-									viewBox="0 0 24 24"
-								>
-									<path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-								</svg>
-							}
-						/>
-						<a href="twitter.com/home" className="ml-3 text-gray-400">
-							<svg
-								fill="currentColor"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								className="w-5 h-5"
-								viewBox="0 0 24 24"
-							>
-								<path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-							</svg>
-						</a>
-						<a className="ml-3 text-gray-400">
-							<svg
-								fill="none"
-								stroke="currentColor"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								className="w-5 h-5"
-								viewBox="0 0 24 24"
-							>
-								<rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-								<path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-							</svg>
-						</a>
-						<a className="ml-3 text-gray-400">
-							<svg
-								fill="currentColor"
-								stroke="currentColor"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="0"
-								className="w-5 h-5"
-								viewBox="0 0 24 24"
-							>
-								<path
-									stroke="none"
-									d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-								></path>
-								<circle cx="4" cy="4" r="2" stroke="none"></circle>
-							</svg>
-						</a> */}
           </div>
         </div>
         <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
@@ -200,27 +141,6 @@ const Footer = () => {
                   />
                 </li>
               ))}
-              {/* <li>
-								<TideLink
-									href="/trade"
-									className="text-gray-400 hover:text-slate-50"
-									content={'Trade'}
-								/>
-							</li>
-							<li>
-								<TideLink
-									href="#"
-									className="text-gray-400 hover:text-slate-50"
-									content={'TideBit University'}
-								/>
-							</li>
-							<li>
-								<TideLink
-									href="#"
-									className="text-gray-400 hover:text-slate-50"
-									content={'Help Center'}
-								/>
-							</li> */}
             </nav>
           </div>
           <div className="lg:w-1/6 md:w-1/2 w-full px-4">
@@ -237,13 +157,6 @@ const Footer = () => {
                   />
                 </li>
               ))}
-              {/* <li>
-								<TideLink
-									href="#"
-									className="text-gray-400 hover:text-slate-50"
-									content={'Hiring'}
-								/>
-							</li> */}
             </nav>
           </div>
           <div className="lg:w-1/6 md:w-1/2 w-full px-4">
@@ -260,20 +173,6 @@ const Footer = () => {
                   />
                 </li>
               ))}
-              {/* <li>
-								<TideLink
-									href="#"
-									className="text-gray-400 hover:text-slate-50"
-									content={'Service policy'}
-								/>
-							</li>
-							<li>
-								<TideLink
-									href="#"
-									className="text-gray-400 hover:text-slate-50"
-									content={'Privacy policy'}
-								/>
-							</li> */}
             </nav>
           </div>
           <div className="lg:w-1/3 lg:ml-20 md:w-1/2 w-full px-4 md:-mt-8 lg:mt-0">
@@ -282,12 +181,6 @@ const Footer = () => {
             </h2>
             <div className="flex flex-wrap justify-center items-end md:justify-start space-y-2">
               <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
-                {/* <label
-									htmlFor="footer-field"
-									className="leading-7 text-sm text-gray-400 text-start"
-								>
-									Placeholder
-								</label> */}
                 <input
                   type="text"
                   id="email"
@@ -296,33 +189,14 @@ const Footer = () => {
                 />
               </div>
               <TideButton isFocus={true} content={`subscribe`.toUpperCase()} />
-              {/* <button className="md:mt-2 lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-								Button
-							</button> */}
             </div>
-            {/* <p className="text-gray-500 text-sm mt-2 md:text-left text-center">
-							Bitters chicharrones fanny pack
-							<br className="lg:block hidden" />
-							waistcoat green juice
-						</p> */}
           </div>
         </div>
       </div>
 
-      {/* TODO: TideBit Trade Mark */}
       <div className="bg-black-800 bg-opacity-75 md:pt-0">
         <div className="pt-1 pb-4 px-5 flex flex-wrap flex-col sm:flex-row sm:justify-center md:justify-end">
-          <p className="text-gray-400 text-sm text-center sm:text-left lg:mr-36">
-            TideBit © 2022
-            {/* <a
-							href="https://twitter.com/knyttneve"
-							rel="noopener noreferrer"
-							className="text-gray-500 ml-1"
-							target="_blank"
-						>
-							@knyttneve
-						</a> */}
-          </p>
+          <p className="text-gray-400 text-sm text-center sm:text-left lg:mr-36">TideBit © 2022</p>
         </div>
       </div>
     </footer>

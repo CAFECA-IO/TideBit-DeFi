3ca69a3a2dbb25e8eecdebda7a131fb41e1023d6
arealclimber <luffulgence@gmail.com>
2022-11-14 18:06:12 +0800

    [statistic] remove redundant code

--- src/components/layout/statistic.js
+++ src/components/layout/statistic.js
@@ -16,33 +16,6 @@ const StatisticBlock = () => {
               </div>
             </div>
           ))}
-          {/* <div className="lg:w-1/3 w-1/3 lg:mb-0 mb-6 p-4">
-						<div className="h-full text-start">
-							<p className="leading-relaxed text-lg">24h volumn on TideBit</p>
-							<h2 className="title-font font-medium text-3xl text-white">
-								365 Billion
-							</h2>
-						</div>
-					</div>
-					<div className="lg:w-1/3 w-1/3 lg:mb-0 mb-6 p-4">
-						<div className="h-full text-start">
-							<p className="leading-relaxed text-lg">Users on TideBit</p>
-
-							<h2 className="title-font font-medium text-3xl text-white">
-								30 Billion+
-							</h2>
-						</div>
-					</div>
-					<div className="lg:w-1/3 w-1/3 lg:mb-0 p-4">
-						<div className="h-full text-start">
-							<p className="leading-relaxed text-lg">Lowest Fee</p>
-
-							<h2 className="title-font font-medium text-3xl text-white">
-								{' '}
-								{`< 0.10%`}
-							</h2>
-						</div>
-					</div> */}
         </div>
       </div>
     </section>

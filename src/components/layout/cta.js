9686338eec481f7da7ba5ca0563c3997fb335582
arealclimber <luffulgence@gmail.com>
2022-11-18 14:00:03 +0800

    test

--- src/components/layout/cta.js
+++ src/components/layout/cta.js
@@ -2,21 +2,24 @@ import React from 'react';
 import Image from 'next/image';
 import TideButton from '../shared/button/tide_button';
 // [url('/public/elements/Group_15198@2x.png')]
+
+// <div className="absolute bottom-[10rem] left-[2rem] bg-blue-400 px-4 text-lg text-white">
+//       <p>Do you want to get notified when a new component is added to Flowbite?</p>
+// </div>
 const Cta = () => {
   return (
-    <section className="h-[800px] w-screen bg-black  bg-landingPageCta1 object-fill text-gray-400">
-      <div className="pl-[150px]">
-        <div className="top-[5rem] left-[2rem] text-white md:top-[20rem] md:left-[12rem] ">
-          <div className="max-w-lg text-3xl font-bold leading-[60px] tracking-wide md:max-w-3xl md:text-7xl">
-            <span className="text-blue-500">Trusted</span> platform for Crypto investment
-          </div>
-          <div className="mt-[55px] max-w-md text-[20px] text-lightGray md:max-w-2xl">
-            Start investing now. On TideBit you can learn, buy and sell cryptocurrency assets with
-            the best quality.{' '}
-          </div>
-          <div className="pt-8">
-            <TideButton content={`Get Started`.toUpperCase()} />
-          </div>
+    <section className="relative w-screen pl-[50px]">
+      <img className="" src="/elements/Group 15198@2x.png" alt="image description" />
+      <div className="absolute top-[20rem] left-[8rem] flex flex-col justify-start">
+        <div className="max-w-lg text-3xl font-bold leading-[60px] tracking-wide text-white md:max-w-3xl md:text-7xl">
+          <span className="text-tidebitTheme">Trusted</span> platform for Crypto investment
+        </div>
+        <div className="mt-[55px] max-w-md text-[20px] text-lightGray md:max-w-2xl">
+          Start investing now. On TideBit you can learn, buy and sell cryptocurrency assets with the
+          best quality.{' '}
+        </div>
+        <div className="pt-8">
+          <TideButton content={`Get Started`.toUpperCase()} />
         </div>
       </div>
     </section>

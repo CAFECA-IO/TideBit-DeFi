526a36a62afac8440d237a721cfd7f0fcc0d2be0
arealclimber <luffulgence@gmail.com>
2022-11-19 21:55:50 +0800

    WORKAROUND for bellicon

--- src/components/layout/nav_bar.js
+++ src/components/layout/nav_bar.js
@@ -46,58 +46,76 @@ const NavBar = ({notificationNumber = 1}) => {
   );
 
   const isDisplayedMobileNavBar = navOpen ? '' : 'hidden';
-
+  // TODO: Pass the sidebar as the props into the blur background
   const isDisplayedNotificationSidebar = componentVisible ? (
     <>
       {/* TODO: Blur background done */}
-      {/* cover */}
-      <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-hidden bg-darkGray/10 outline-none backdrop-blur-sm focus:outline-none">
-        {/* sidebar section */}
-        <div className="fixed right-1 top-10 z-10 flex overflow-x-hidden overflow-y-hidden outline-none focus:outline-none">
-          <div className="relative my-6 mx-auto w-auto max-w-xl">
-            {' '}
-            <div className={`relative`}>
-              {/* sidebar self */}
-              <div
-                ref={ref}
-                className={`${'w-[479px]'} h-screen bg-darkGray/90 p-5 pt-8 text-white duration-300`}
-              >
-                <h1 className="pl-5 text-[25px] font-bold">Notification</h1>
-                <div className="fixed right-[30px] text-[14px] text-tidebitTheme underline hover:cursor-pointer">
-                  Clear All
-                </div>
-                <div className="mt-[83px] -mb-[28px] flex">
-                  <span className="mx-2 inline-block h-[158px] w-[5px] flex-shrink-0 bg-tidebitTheme"></span>
-                  <div className="-mt-[130px] flex items-center">
-                    <Image
-                      className="ml-[8px] -mt-[10px] flex flex-shrink-0"
-                      src="/elements/megaphone.svg"
-                      width={30}
-                      height={26}
-                      alt="icon"
-                    />
-                    <div className="relative mt-[88px] ml-3 text-start">
-                      <div className="">
-                        <div className="absolute top-[40px] text-[25px] text-lightWhite">
-                          Happy Birthday to TideBit
-                        </div>
-                        <div className="right-[20px] pt-[42px] pl-[300px] pb-[50px] text-end text-[12px] text-lightGray">
-                          <div>2022-10-05</div>
-                          <div>14:28:38</div>
-                        </div>
-                      </div>
 
-                      <div className="mb-[23px] -mt-[30px] flex flex-wrap text-[12px] text-lightGray">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-                        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-                        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-                        clita kasd gubergren, no sea takimata sanctus est Lorem
+      {/* cover of NavBar ***Bell Icon*** */}
+      <div
+        className={`${
+          componentVisible ? 'transition-opacity duration-700 ease-in-out' : 'invisible'
+        } invisible fixed z-50 flex h-16 items-center justify-center overflow-x-hidden overflow-y-hidden bg-transparent outline-none hover:cursor-pointer focus:outline-none lg:visible lg:right-52 lg:top-6 lg:h-8 lg:w-8`}
+      >
+        {' '}
+      </div>
+
+      {/* cover o Main, Footer */}
+      <div
+        className={`${
+          componentVisible ? 'transition-opacity duration-700 ease-in-out' : 'invisible'
+        } fixed inset-0 z-30 flex items-center justify-center overflow-x-hidden overflow-y-hidden bg-darkGray/10 outline-none backdrop-blur-sm focus:outline-none`}
+      >
+        {' '}
+      </div>
+
+      {/* sidebar section */}
+      <div className="fixed right-1 top-[44px] z-50 flex overflow-x-hidden overflow-y-hidden outline-none focus:outline-none">
+        <div className="relative my-6 mx-auto w-auto max-w-xl">
+          {' '}
+          <div className={`relative`}>
+            {/* sidebar self */}
+            <div
+              ref={ref}
+              className={`${'w-[479px]'} h-screen ${
+                componentVisible ? 'translate-x-0' : 'translate-x-[479px]'
+              } bg-darkGray/90 p-5 pt-8 text-white transition-all duration-500 ease-in-out`}
+            >
+              <h1 className="pl-5 text-[25px] font-bold">Notification</h1>
+              <div className="fixed right-[30px] text-[14px] text-tidebitTheme underline hover:cursor-pointer">
+                Clear All
+              </div>
+              <div className="mt-[83px] -mb-[28px] flex">
+                <span className="mx-2 inline-block h-[158px] w-[5px] flex-shrink-0 bg-tidebitTheme"></span>
+                <div className="-mt-[130px] flex items-center">
+                  <Image
+                    className="ml-[8px] -mt-[10px] flex flex-shrink-0"
+                    src="/elements/megaphone.svg"
+                    width={30}
+                    height={26}
+                    alt="icon"
+                  />
+                  <div className="relative mt-[88px] ml-3 text-start">
+                    <div className="">
+                      <div className="absolute top-[40px] text-[25px] text-lightWhite">
+                        Happy Birthday to TideBit
                       </div>
+                      <div className="right-[20px] pt-[42px] pl-[300px] pb-[50px] text-end text-[12px] text-lightGray">
+                        <div>2022-10-05</div>
+                        <div>14:28:38</div>
+                      </div>
+                    </div>
+
+                    <div className="mb-[23px] -mt-[30px] flex flex-wrap text-[12px] text-lightGray">
+                      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+                      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+                      kasd gubergren, no sea takimata sanctus est Lorem
                     </div>
                   </div>
                 </div>
-                <span className="ml-2 inline-block h-[1px] w-[438px] flex-shrink-0 bg-lightGray"></span>
               </div>
+              <span className="ml-2 inline-block h-[1px] w-[438px] flex-shrink-0 bg-lightGray"></span>
             </div>
           </div>
         </div>
@@ -108,7 +126,7 @@ const NavBar = ({notificationNumber = 1}) => {
   return (
     <>
       <div className="w-full bg-black">
-        <nav className="container fixed inset-x-0 z-50 mx-auto max-w-full bg-black/100 pb-1 text-white">
+        <nav className="container fixed inset-x-0 z-40 mx-auto max-w-full bg-black/100 pb-1 text-white">
           <div className="mx-auto max-w-full px-8">
             <div className="flex h-16 items-center justify-between">
               <div className="flex items-center">
@@ -236,7 +254,7 @@ const NavBar = ({notificationNumber = 1}) => {
                   </div>
                   {/* <TbMinusVertical size={30} className="" /> */}
                   <span className="mx-2 inline-block h-10 w-px rounded bg-lightGray1"></span>
-
+                  {/*FIXME: mobile notify icon*/}
                   <div onClick={sidebarOpenHandler} className="relative hover:cursor-pointer">
                     <span className="absolute top-0 -right-1 z-20 inline-block h-3 w-3 rounded-xl bg-cyan-300">
                       <p className="text-center text-[8px] hover:text-white">

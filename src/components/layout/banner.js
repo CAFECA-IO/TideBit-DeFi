c9735389e8d851347b7466734b5a5b4a241d7260
arealclimber <luffulgence@gmail.com>
2022-11-21 15:22:29 +0800

    banner trivia

--- src/components/layout/banner.js
+++ src/components/layout/banner.js
@@ -2,14 +2,15 @@ import Image from 'next/image';
 import React from 'react';
 import TideButton from '../shared/button/tide_button';
 // mx-auto flex flex-col items-center justify-center px-5 py-24
+// pb-[600px]
 const Banner = () => {
   return (
-    <section className="w-screen bg-darkGray3">
+    <section className="h-fit w-screen md:mb-40">
       <div className="">
-        <div className="pb-[600px]">
+        <div className="">
           <div className="relative">
             <Image
-              className="absolute top-0 left-0 bg-transparent opacity-80"
+              className="absolute top-0 left-0 h-fit bg-darkGray3 opacity-80"
               alt="hero"
               src="/elements/group_14583.svg"
               width={1920}
@@ -22,7 +23,7 @@ const Banner = () => {
               src="/elements/group_15199@2x.png"
               contain="true"
             />
-            <div className="absolute left-16 top-0 mt-5 flex text-xs xxs:left-20 xxs:mt-7 xxs:text-sm xs:left-28 xs:mt-10 xs:text-base sm:left-36 sm:top-3 sm:text-xl md:left-40 md:top-5 md:mt-12 md:text-3xl lg:top-5 lg:left-52 lg:mt-16 lg:text-4xl xl:top-10 xl:left-56 xl:text-5xl 2xl:left-72 2xl:text-6xl">
+            <div className="absolute left-16 top-0 mt-5 flex text-xs xxs:left-20 xxs:mt-7 xxs:text-sm xs:left-28 xs:mt-10 xs:text-base sm:left-36 sm:top-3 sm:text-xl md:left-40 md:top-5 md:mt-12 md:text-3xl lg:top-5 lg:left-52 lg:mt-16 lg:text-4xl xl:top-5 xl:left-56 xl:text-5xl 2xl:left-72 2xl:text-6xl">
               <div className="flex flex-col items-start justify-start space-y-2 text-start font-bold sm:space-y-4 lg:space-y-8">
                 <div className="text-white">New Wallet available</div>
                 <div className="text-tidebitTheme">iSunOne</div>

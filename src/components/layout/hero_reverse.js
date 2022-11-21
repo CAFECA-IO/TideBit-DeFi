72d5585dc51c58d2e4f819f060f7e8e7b26337f0
arealclimber <luffulgence@gmail.com>
2022-11-21 11:20:33 +0800

    banner trivial

--- src/components/layout/hero_reverse.js
+++ src/components/layout/hero_reverse.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // ml-20 & mr-40 are used to center the content
-const HeroReverse = ({heading, content, ...otherProps}) => {
+const HeroReverse = ({heading, content, img, ...otherProps}) => {
   const displayedHeading = heading ? (
     heading
   ) : (
@@ -18,16 +18,20 @@ cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote
 bag selvage hot chicken authentic tumeric truffaut hexagon try-hard
 chambray.`;
 
+  const displayedImg = img ? (
+    img
+  ) : (
+    <img
+      className="rounded object-cover object-center"
+      alt="hero"
+      src="https://dummyimage.com/500x400"
+    />
+  );
+
   return (
     <section className="bg-black text-gray-400">
       <div className="container mx-auto flex flex-col items-center px-5 pb-24 md:flex-row">
-        <div className="mb-10 w-5/6 md:mb-0 md:w-1/2 lg:w-full lg:max-w-lg">
-          <img
-            className="rounded object-cover object-center"
-            alt="hero"
-            src="https://dummyimage.com/500x400"
-          />
-        </div>
+        <div className="mb-10 w-5/6 md:mb-0 md:w-1/2 lg:w-full lg:max-w-lg">{displayedImg}</div>
         <div className="flex flex-col items-center text-center md:w-1/2 md:items-start md:pl-16 md:text-left lg:flex-grow lg:pl-24 xl:ml-20">
           <h1 className="mb-4 text-3xl font-medium text-white sm:text-3xl md:text-center">
             {displayedHeading}

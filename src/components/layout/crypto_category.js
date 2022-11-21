3df07a1c3287ca496bc7195fa657db14a6a49f94
arealclimber <luffulgence@gmail.com>
2022-11-21 11:41:58 +0800

    crypto category trifles

--- src/components/layout/crypto_category.js
+++ src/components/layout/crypto_category.js
@@ -6,14 +6,14 @@ const CryptoCategory = ({...otherProps}) => {
     <div
       className={`${customClassName} container mx-auto mt-20 flex flex-wrap justify-center space-y-1`}
     >
-      <div className="mb-20 flex w-full flex-col text-center">
-        <div className="mb-4 items-center text-3xl font-medium text-white sm:text-4xl">
+      <div className="mb-5 flex w-full flex-col text-center">
+        <div className="mb-0 items-center text-3xl font-medium text-white sm:text-4xl">
           <div className="flex items-center justify-center">
-            <span className="my-auto hidden h-px rounded bg-white md:inline-block lg:w-1/5 xl:mx-2 xl:w-1/3"></span>
+            <span className="my-auto hidden h-px rounded bg-white/50 md:inline-block lg:w-1/5 xl:mx-2 xl:w-1/3"></span>
             <h1 className="mx-1 text-center xl:w-[410px]">
               <span className="text-tidebitTheme">Popular</span> cryptocurrencies
             </h1>
-            <span className="my-auto hidden h-px rounded bg-white md:inline-block lg:w-1/5 xl:mx-2 xl:w-1/3"></span>
+            <span className="my-auto hidden h-px rounded bg-white/50 md:inline-block lg:w-1/5 xl:mx-2 xl:w-1/3"></span>
           </div>
         </div>
       </div>

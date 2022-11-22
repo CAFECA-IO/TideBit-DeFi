2a4ad17611c784c8d688f21d6e3ff0b3dabd78ab
arealclimber <luffulgence@gmail.com>
2022-11-23 01:10:24 +0800

    connect placeholder

--- src/components/connecting_modal/connecting_modal.js
+++ src/components/connecting_modal/connecting_modal.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import {ImCross} from 'react-icons/im';
 
 // TODO: Loading component
 // TODO: Procedure component
 // TODO: Signature success or not
 
-export default function ConnectingModal({
+/**
   connecting = false,
   connected = false,
   error = false,
   signing = false,
   signature = false,
   ...otherProps
-}) {
+ */
+
+export default function ConnectingModal() {
+  const [loading, setLoading] = useState(false);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [signature, setSignature] = useState(null);
+  const [userBalance, setUserBalance] = useState(null);
+
   return (
     <>
       <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
@@ -23,56 +31,33 @@ export default function ConnectingModal({
             className="relative flex w-full flex-col rounded-lg border-0 bg-darkGray1 shadow-lg outline-none focus:outline-none"
           >
             {/*header*/}
-            <div className="flex items-start justify-between rounded-t pt-8">
-              <h3 className="mb-3 mt-2 ml-auto text-4xl font-semibold text-lightWhite">
+            <div className="flex items-start justify-between rounded-t pt-6 ">
+              <h3 className="mx-auto mt-2 w-20rem pl-1/8 text-4xl font-semibold text-lightWhite">
                 Wallet Connect
               </h3>
               <button className="float-right ml-auto border-0 bg-transparent p-1 text-base font-semibold leading-none text-gray-300 outline-none focus:outline-none">
-                <span className="mx-8 my-4 block h-6 w-6 outline-none focus:outline-none">
+                <span className="absolute top-5 right-5 block outline-none focus:outline-none">
                   <ImCross />
                 </span>
               </button>
             </div>
             {/*body*/}
-            <div className="relative mx-10 flex-auto p-6">
+            <div className="relative mx-10 flex-auto px-4 pb-4 pt-1">
               <div className="my-4 text-lg leading-relaxed text-white">
-                <div className="grid grid-cols-3 gap-3">
-                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2">
-                    Test
-                  </div>
-                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2">
-                    Test
-                  </div>
-                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2">
-                    Test
-                  </div>
-                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2">
-                    Test
-                  </div>
-                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2">
-                    Test
-                  </div>
-                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2">
-                    Test
-                  </div>
-                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2">
-                    Test
-                  </div>
-                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2">
-                    Test
-                  </div>
-                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2">
-                    Test
-                  </div>
-                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2">
-                    Test
-                  </div>
-                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2">
-                    Test
-                  </div>
-                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2">
-                    Test
-                  </div>
+                <div className="flex justify-center">
+                  <div className="text-2xl">Loading Circle Animation</div>
+                  {/* <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2"></div>
+                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2"></div>
+                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2"></div>
+                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2"></div>
+                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2"></div>
+                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2"></div>
+                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2"></div>
+                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2"></div>
+                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2"></div>
+                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2"></div>
+                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2"></div>
+                  <div className="col-span-1 flex items-center justify-center rounded bg-darkGray2"></div> */}
                 </div>
               </div>
             </div>

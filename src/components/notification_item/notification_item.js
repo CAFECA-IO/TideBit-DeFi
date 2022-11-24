f865a86732bc65e39e33d946889b010d151ebeea
arealclimber <luffulgence@gmail.com>
2022-11-24 12:16:52 +0800

    test speaker ok

--- src/components/notification_item/notification_item.js
+++ src/components/notification_item/notification_item.js
@@ -20,22 +20,25 @@ export default function NotificationItem({title = '', date = '', time = '', cont
       <div className="flex">
         {/* Vertical line */}
         <span className="mx-2 inline-block h-158px w-5px shrink-0 bg-tidebitTheme"></span>
+
         {/* Speaker & Heading & Content & Date */}
         <div className="flex items-center">
-          <Image
-            className="ml-8px mb-10px flex shrink-0"
-            src="/elements/megaphone.svg"
-            width={30}
-            height={26}
-            alt="icon"
-          />
+          <div>
+            <Image
+              className="ml-8px mb-10px flex shrink-0"
+              src="/elements/megaphone.svg"
+              width={30}
+              height={26}
+              alt="icon"
+            />
 
-          <div className="relative mt-88px ml-3 text-start">
-            <div className="">
-              <div className="absolute top-40px text-2xl text-lightWhite">{title}</div>
-              <div className="right-20px pl-300px pt-42px pb-50px text-end text-xs text-lightGray">
-                <div>{date}</div>
-                <div>{time}</div>
+            <div className="relative mt-88px ml-3 text-start">
+              <div className="">
+                <div className="absolute top-40px text-2xl text-lightWhite">{title}</div>
+                <div className="right-20px pl-300px pt-42px pb-50px text-end text-xs text-lightGray">
+                  <div>{date}</div>
+                  <div>{time}</div>
+                </div>
               </div>
             </div>
 

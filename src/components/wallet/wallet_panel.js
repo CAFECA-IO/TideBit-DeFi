423e2eae26fb74756b37a5855556aef268443a64
arealclimber <luffulgence@gmail.com>
2022-11-25 19:03:41 +0800

    processssssssssssssssssssssssssssssssssssss

--- src/components/wallet/wallet_panel.js
+++ src/components/wallet/wallet_panel.js
@@ -61,7 +61,242 @@ export default function WalletPanel(props) {
     // console.log('wallet connect option clicked');
   };
 
+  const processClickHandler = () => {
+    setProcessModalVisible(!processModalVisible);
+  };
+
+  // TODO: Process Modal Controllerrrrrrrrr
+  // SignatureProcess
+  const processModalController = ({
+    loading = true,
+    firstStepSuccess = false,
+    firstStepError = false,
+    secondStepSuccess = false,
+    secondStepError = false,
+  }) => (
+    <SignatureProcess
+      loading={loading}
+      firstStepSuccess={firstStepSuccess}
+      firstStepError={firstStepError}
+      secondStepSuccess={secondStepSuccess}
+      secondStepError={secondStepError}
+    />
+  );
+
   // const isDisplayedQrcodeModal =
+
+  function SignatureProcess({
+    loading = false,
+    firstStepSuccess = false,
+    firstStepError = false,
+    secondStepSuccess = false,
+    secondStepError = false,
+  }) {
+    const controlSpace = firstStepError || secondStepError ? 'space-y-12' : 'space-y-16';
+
+    const firstStepIcon = (
+      <Image src="/elements/group_2415.svg" width={32} height={32} alt="step 1 icon" />
+    );
+
+    const successIcon = (
+      <Image src="/elements/group_2415_check.svg" width={32} height={32} alt="successful icon" />
+    );
+
+    const errorIcon = (
+      <Image src="/elements/group_2418_error.svg" width={32} height={32} alt="error icon" />
+    );
+
+    const secondStepDefaultIcon = (
+      <Image src="/elements/group_2418.svg" width={32} height={32} alt="step 2 icon" />
+    );
+
+    const secondStepActivatedIcon = (
+      <Image src="/elements/group_2418(1).svg" width={32} height={32} alt="step 2 icon" />
+    );
+
+    const requestButtonHandler = loading ? (
+      <Lottie className="w-40px" animationData={smallConnectingAnimation} />
+    ) : (
+      <TideButton className="px-5" content={`Send Requests`} />
+    );
+
+    const firstStepDefaultView = (
+      <>
+        <div>{firstStepIcon}</div>
+        <div className="w-271px space-y-1 text-lightWhite">
+          <div className="text-lg">Verify ownership</div>
+          <div className="text-sm">Confirm you are the owner of this wallet</div>
+        </div>
+      </>
+    );
+
+    const firstStepSuccessView = (
+      <>
+        <div>{successIcon}</div>
+        <div className="w-271px space-y-1 text-lightWhite">
+          <div className="text-lg">Verify ownership</div>
+          <div className="text-sm">Confirm you are the owner of this wallet</div>
+        </div>
+      </>
+    );
+
+    const firstStepErrorView = (
+      <>
+        <div>{errorIcon}</div>
+        <div className="w-271px space-y-1 text-lightWhite">
+          <div className="text-lg">Verify ownership</div>
+          <div className="text-sm">Confirm you are the owner of this wallet</div>
+          <div className="text-sm text-lightRed3">Something went wrong, please try again</div>
+        </div>
+      </>
+    );
+
+    const firstStepSectionHandler = (
+      // {firstStepSuccess ? 'success section' : firstStepError ? 'error section' : 'default section'}
+      <>
+        {firstStepSuccess
+          ? firstStepSuccessView
+          : firstStepError
+          ? firstStepErrorView
+          : firstStepDefaultView}
+      </>
+    );
+
+    const secondStepDefaultView = (
+      <>
+        <div>{secondStepDefaultIcon}</div>
+        <div className="w-271px space-y-1 text-lightGray">
+          <div className="text-lg">Enable trading</div>
+          <div className="text-sm">
+            Enable secure access to our API for lightning quick trading.
+          </div>
+        </div>
+      </>
+    );
+    const secondStepActiveView = (
+      <>
+        <div>
+          {' '}
+          {secondStepActivatedIcon}
+          {/* <Image src="/elements/group_2418(1).svg" width={32} height={32} alt="step 2 icon" /> */}
+        </div>
+        <div className="w-271px space-y-1 text-lightWhite">
+          <div className="text-lg">Enable trading</div>
+          <div className="text-sm">
+            Enable secure access to our API for lightning quick trading.
+          </div>
+        </div>
+      </>
+    );
+
+    const secondStepSuccessView = (
+      <>
+        <div>
+          {successIcon}{' '}
+          {/* <Image src="/elements/group_2418(1).svg" width={32} height={32} alt="step 2 icon" /> */}
+        </div>
+        <div className="w-271px space-y-1 text-lightWhite">
+          <div className="text-lg">Enable trading</div>
+          <div className="text-sm">
+            Enable secure access to our API for lightning quick trading.
+          </div>
+        </div>
+      </>
+    );
+
+    const secondStepErrorView = (
+      <>
+        <div>
+          {' '}
+          {errorIcon}
+          {/* <Image src="/elements/group_2418(1).svg" width={32} height={32} alt="step 2 icon" /> */}
+        </div>
+        <div className="w-271px space-y-1 text-lightWhite">
+          <div className="text-lg">Enable trading</div>
+          <div className="text-sm">
+            Enable secure access to our API for lightning quick trading.
+          </div>
+          <div className="text-sm text-lightRed3">Something went wrong, please try again</div>
+        </div>
+      </>
+    );
+
+    const secondStepSectionHandler = (
+      // {/* {secondStepSuccess ? 'success section' : secondStepError ? 'error section' : firstStepSuccess ? 'active section' :  'default section'} */}
+      <>
+        {secondStepSuccess
+          ? secondStepSuccessView
+          : secondStepError
+          ? secondStepErrorView
+          : firstStepSuccess
+          ? secondStepActiveView
+          : secondStepDefaultView}
+      </>
+    );
+
+    return processModalVisible ? (
+      <>
+        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
+          <div className="relative my-6 mx-auto w-auto max-w-xl">
+            {/*content & panel*/}
+            <div
+              id="connectModal"
+              ref={processModalRef}
+              className="relative flex h-600px w-450px flex-col rounded-3xl border-0 bg-darkGray1 shadow-lg shadow-black/80 outline-none focus:outline-none"
+            >
+              {/*header*/}
+              <div className="flex items-start justify-between rounded-t pt-6">
+                <h3 className="ml-1/8 mt-2 w-20rem pl-1/8 text-4xl font-semibold text-lightWhite">
+                  Wallet Connect
+                </h3>
+                <button className="float-right ml-auto border-0 bg-transparent p-1 text-base font-semibold leading-none text-gray-300 outline-none focus:outline-none">
+                  <span className="absolute top-5 right-5 block outline-none focus:outline-none">
+                    <ImCross onClick={processClickHandler} />
+                  </span>
+                </button>
+              </div>
+              {/*body*/}
+
+              <div className="flex flex-auto flex-col items-center pt-5">
+                <div className="text-lg leading-relaxed text-lightWhite">
+                  <div className="mx-auto flex flex-col items-center">
+                    <div className="mt-8 text-center text-lg text-lightGray">
+                      <div>You will receive two signature requests.</div>
+                      <div>
+                        {' '}
+                        Signing is{' '}
+                        <span className="text-tidebitTheme">
+                          <Link href="#">free</Link>
+                        </span>{' '}
+                        and will not send a transaction.
+                      </div>
+                    </div>
+
+                    {/* Activate First Step */}
+                    <div className={`${controlSpace} flex flex-col pt-16`}>
+                      <div className="flex items-center justify-center space-x-3">
+                        {firstStepSectionHandler}
+                      </div>
+
+                      {/* Second Step */}
+                      <div className="flex items-center justify-center space-x-3">
+                        {secondStepSectionHandler}
+                      </div>
+                    </div>
+
+                    <div className="mt-16">{requestButtonHandler}</div>
+                  </div>
+                </div>
+              </div>
+              {/*footer*/}
+              <div className="flex items-center justify-end rounded-b p-2"></div>
+            </div>
+          </div>
+        </div>
+        <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+      </>
+    ) : null;
+  }
 
   function QrcodeModal() {
     return qrcodeModalVisible ? (
@@ -158,19 +393,19 @@ export default function WalletPanel(props) {
     ) : null;
   }
 
-  const isLoading = loading ? <DisplayedConnecting /> : null;
+  const isConnecting = connecting ? <DisplayedConnecting /> : null;
 
   // const isWalletConnectOpen =
 
   const walletOptionClickHandler = async () => {
-    setComponentVisible(!componentVisible);
-    setLoading(true);
-    setConnectingModalVisible(true);
-
     // TODO: NNNNNNNotes
     // console.log('connecting modal should be visible: ', connectingModalVisible);
 
     try {
+      setComponentVisible(!componentVisible);
+      setConnecting(true);
+      setConnectingModalVisible(true);
+
       let provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
       // pop up the metamask window
       await provider.send('eth_requestAccounts', []);
@@ -185,17 +420,24 @@ export default function WalletPanel(props) {
 
       // console.log('connect to Metamask clicked, Account: ', address);
 
+      // TODO: NNNNNNNotes
+      // console.log('connecting modal should be invisible: ', connectingModalVisible);
+      setConnectingModalVisible(false);
+      setConnecting(false);
+
+      setProcessModalVisible(true);
+      // processModalController({loading: true});
+      <SignatureProcess loading={true} />;
+
       let signature = await signer.signMessage('TideBit DeFi test');
       // console.log('Sign the message, get the signature is: ', signature);
     } catch (error) {
       // console.log(error);
       setErrorMessages(error);
-    }
 
-    // TODO: NNNNNNNotes
-    // console.log('connecting modal should be invisible: ', connectingModalVisible);
-    setConnectingModalVisible(false);
-    setLoading(false);
+      setConnectingModalVisible(false);
+      setConnecting(false);
+    }
   };
 
   // click metamask => connect to metamask & show connecting modal
@@ -342,8 +584,11 @@ export default function WalletPanel(props) {
       {isDisplayedWalletPanel}
 
       {isDisplayedConnectingModal}
-      {isLoading}
+      {isConnecting}
       <QrcodeModal />
+
+      {/* <processModalController loading={true} /> */}
+      <SignatureProcess />
       {/* {isDisplayedQrcodeModal} */}
       {/* <ConnectingModal /> */}
     </>

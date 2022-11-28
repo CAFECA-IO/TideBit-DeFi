24e20d2dd247302ec823dc226abfd10324ef0e9e
arealclimber <luffulgence@gmail.com>
2022-11-28 12:28:27 +0800

    image alt added

--- src/components/hero_description/hero_description.js
+++ src/components/hero_description/hero_description.js
@@ -33,7 +33,7 @@ export default function HeroDescription() {
           heading={`Web`}
           highlight={`3.0`}
           content={`To fit in the new generation, TideBit uses Web 3.0 technology. Make it easy to buy and sell crypto currency.`}
-          img={<Image src="/elements/2634@2x.png" width={976} height={588} />}
+          img={<Image src="/elements/2634@2x.png" width={976} height={588} alt="picture" />}
         />
         <div className="py-5 lg:py-40"></div>
 
@@ -46,7 +46,7 @@ export default function HeroDescription() {
             </div>
           }
           content={`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam`}
-          img={<Image src="/elements/group_15200@2x.png" width={1252} height={879} />}
+          img={<Image src="/elements/group_15200@2x.png" width={1252} height={879} alt="picture" />}
         />
         <div className="py-5 lg:py-40"></div>
 
@@ -59,7 +59,7 @@ export default function HeroDescription() {
             </div>
           }
           content={`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam`}
-          img={<Image src="/elements/group_15145@2x.png" width={1366} height={930} />}
+          img={<Image src="/elements/group_15145@2x.png" width={1366} height={930} alt="picture" />}
         />
         <div className="py-5 lg:py-40"></div>
 
@@ -72,7 +72,7 @@ export default function HeroDescription() {
             </div>
           }
           content={`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam`}
-          img={<Image src={`/elements/group_15201.svg`} width={1364} height={820} />}
+          img={<Image src={`/elements/group_15201.svg`} width={1364} height={820} alt="picture" />}
         />
         <div className="py-5 lg:py-10"></div>
 

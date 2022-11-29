25abbf7566e0445c3c2403dbcd69d62d8d4b435a
arealclimber <luffulgence@gmail.com>
2022-11-30 02:01:07 +0800

    line graph

--- src/components/card/crypto_card.js
+++ src/components/card/crypto_card.js
@@ -1,8 +1,10 @@
 import Image from 'next/image';
 import React from 'react';
-import {FaEthereum} from 'react-icons/fa';
-// import {ReactComponent as ethIcon} from '/public/elements/group_15143.svg';
-// import {ReactComponent as Logo} from './logo.svg';
+import LineGraph from '../line_graph/line_graph';
+// import {FaEthereum} from 'react-icons/fa';
+// // import {ReactComponent as ethIcon} from '/public/elements/group_15143.svg';
+// // import {ReactComponent as Logo} from './logo.svg';
+// import LineGraph from '../line_graph/line_graph';
 
 /**
  * @dev used when it needs the star functionality
@@ -28,7 +30,10 @@ const CryptoCard = ({
   // console.log('fluctuating', fluctuating);
   let priceRise = fluctuating > 0 ? true : false;
   let fluctuatingRate = priceRise ? `(+${fluctuating}%)▴` : `(${fluctuating}%)▾`;
-  const priceColor = priceRise ? `text-lightGreen` : `text-lightRed`;
+  // TODO: input the data and price color change as props
+  let priceColor = priceRise ? `text-lightGreen` : `text-lightRed`;
+  // let priceColor = '';
+
   // console.log('priceColor', priceColor);
 
   // const upSvg = (
@@ -43,30 +48,57 @@ const CryptoCard = ({
   //   </svg>
   // );
 
-  // const EthIconDownloadOnline = (
-  //   <FaEthereum
-  //     size={30}
-  //     className="absolute top-1/2 left-1/2 h-5 -translate-x-1/2 -translate-y-1/2 text-white"
-  //   />
-  // );
-
-  // const ImageComponent = (
-  //   <Image src="/elements/group_15143.svg" width={50} height={50} className="text-red-600" />
-  // );
-
-  // const tokenComponentExample = (
-  //   <img src="/elements/c5b7bda06ddfe2b3f59b37ed6bb65ab4.svg" alt="token" />
-  // );
-
-  // const SvgComponent = props => (
-  //   <svg width={48} height={1} xmlns="http://www.w3.org/2000/svg" {...props}>
-  //     <title>{'Crypto Icon'}</title>
-  //     <path d="M0 0h48v1H0z" fill="#243c5a" fillRule="evenodd" />
-  //   </svg>
-  // );
-
   const desktopVersionBreakpoint = 'xs:flex';
   const mobileVersionBreakpoint = 'xs:hidden';
+
+  function randomeNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  function randomArray(min, max, length) {
+    let arr = [];
+    for (let i = 0; i < length; i++) {
+      arr.push(randomeNumber(min, max));
+    }
+    return arr;
+  }
+
+  // function arrayGenerator() {
+  //   let arr = [];
+  //   for (let i = 0; i < 10; i++) {
+  //     arr.push(randomArray(22, 222, 5));
+  //   }
+  //   return arr;
+  // }
+  // console.log(arrayGenerator());
+
+  const sampleArray = randomArray(1100, 1200, 10);
+
+  // TODO: Notes execution order about parameters and logic flow
+  // #1AE2A0 is light green, #E86D6D is light red
+  const fakeDataColor = () => {
+    if (sampleArray[sampleArray.length - 1] > sampleArray[sampleArray.length - 2]) {
+      // priceColor = 'text-lightGreen';
+      return '#1AE2A0';
+    }
+
+    // priceColor = 'text-lightRed';
+    return '#E86D6D';
+  };
+  const thisRandomColor = fakeDataColor();
+
+  // const priceColorDetect = () => {
+  //   if (thisRandomColor === '#1AE2A0') {
+  //     priceColor = 'text-lightGreen';
+  //   } else if (thisRandomColor === '#E86D6D') {
+  //     priceColor = 'text-lightRed';
+  //   }
+  // };
+
+  // priceColorDetect();
+
+  // console.log('randomColor', randomColor());
+
   return (
     <>
       {/* Desktop (width > 500px) version (Card 200x120) */}
@@ -85,11 +117,17 @@ const CryptoCard = ({
 
           {/* line graph & price & fluctuating rate */}
           <div className="flex flex-col justify-start">
-            <div className="relative mt-4 h-2 w-36 rounded bg-gray-200">
-              <div className="absolute top-0 left-0 h-2 w-2/3 rounded bg-blue-200"></div>
+            <div className="absolute top-4 h-96 bg-transparent">
+              <LineGraph
+                sampleArray={sampleArray}
+                strokeColor={thisRandomColor}
+                lineGraphWidth="170"
+              />
+
+              {/* <div className="absolute top-0 left-0 h-2 w-2/3 rounded bg-blue-200"></div> */}
             </div>
             {/**@note no default text color, otherwise it will make actual text color not work */}
-            <div className="relative flex w-200px justify-between">
+            <div className="absolute bottom-0 flex w-200px justify-between">
               <span
                 className={`flex items-center justify-between text-sm ${priceColor} mt-3 align-middle`}
               >
@@ -119,11 +157,16 @@ const CryptoCard = ({
 
           {/* line graph & price & fluctuating rate */}
           <div className="flex flex-col justify-start">
-            <div className="relative mt-1 h-2 w-24 rounded bg-gray-200">
-              <div className="absolute top-0 left-0 h-2 w-2/3 rounded bg-blue-200"></div>
+            <div className="absolute right-0 top-1 bg-transparent">
+              {/* <div className="absolute top-0 left-0 h-2 w-2/3 rounded bg-blue-200"></div> */}
+              <LineGraph
+                sampleArray={sampleArray}
+                strokeColor={fakeDataColor()}
+                lineGraphWidth="140"
+              />
             </div>
             {/**@note no default text color, otherwise it will make actual text color not work */}
-            <div className="relative flex w-134px justify-between">
+            <div className="absolute bottom-0 flex w-134px justify-between">
               <span
                 className={`flex items-center justify-between text-xs ${priceColor} mt-3 align-middle`}
               >
--- src/pages/trial.js
+++ src/pages/trial.js
@@ -1,64 +1,43 @@
-import React, {useState} from 'react';
-import Toast from '../components/toast/toast';
-import ConnectingModal from '../components/wallet/connecting_modal';
-import useOuterClick from '../hooks/lib/use_outer_click';
-import HelloModal from '../components/wallet/hello_modal';
-import QrcodeModal from '../components/wallet/qrcode_modal';
-import SignatureProcessModal from '../components/wallet/signature_process_modal';
+import LineGraph from '../components/line_graph/line_graph';
+import dynamic from 'next/dynamic';
+
+// const Chart = dynamic(() => import('react-apexcharts'), {ssr: false});
+// const Chart = dynamic(() => import('apexcharts'), {ssr: false});
 
 export default function Trial() {
-  const [showToast, setShowToast] = useState(false);
-  const {
-    ref: processModalRef,
-    componentVisible: processModalVisible,
-    setComponentVisible: setProcessModalVisible,
-  } = useOuterClick(true);
+  function randomeNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
-  const processClickHandler = () => {
-    setProcessModalVisible(!processModalVisible);
-  };
+  function randomArray(min, max, length) {
+    let arr = [];
+    for (let i = 0; i < length; i++) {
+      arr.push(randomeNumber(min, max));
+    }
+    return arr;
+  }
 
-  const toastHandler = () => {
-    setShowToast(!showToast);
-  };
+  // console.log(randomArray(22, 60, 10));
 
   return (
-    // <ConnectingModal isShowing={true} />
-    // <HelloModal
-    //   helloModalRef={helloModalRef}
-    //   helloModalVisible={helloModalVisible}
-    //   helloClickHandler={helloClickHandler}
-    // />
-
-    <SignatureProcessModal
-      firstStepSuccess={true}
-      secondStepError={true}
-      processModalRef={processModalRef}
-      processModalVisible={processModalVisible}
-      processClickHandler={processClickHandler}
-    />
-
-    // <QrcodeModal
-    //   qrcodeModalRef={qrcodeModalRef}
-    //   qrcodeModalVisible={qrcodeModalVisible}
-    //   qrcodeClickHandler={qrcodeClickHandler}
-    // />
-
-    // <div className="flex justify-center">
-    //   <button
-    //     onClick={toastHandler}
-    //     className="rounded-xl bg-cuteBlue px-5 py-2 text-black hover:bg-cuteBlue/90"
-    //   >
-    //     showToast Switch
-    //   </button>
-    //   {/* <showToast /> */}
-    //   <Toast
-    //     title="The title"
-    //     content={`Signature: \n 0xf6ecd3ca1eddbee3b25b3371da896d9437ca271408716725bbd33ac2e6c7c9c8215972b41c80d1bfbe7aeb1aad4c0c8ac80fbe4293528daa4ab6da0747fe04fe1c`}
-    //     toastHandler={toastHandler}
-    //     showToast={showToast}
-    //   />
-    //   {/* {isDisplayedToast} */}
-    // </div>
+    <div className="flex justify-center">
+      <div>
+        <LineGraph
+          sampleArray={randomArray(22, 222, 10)}
+          strokeColor="#627eea"
+          lineGraphWidth="120"
+        />
+        <LineGraph
+          sampleArray={randomArray(22, 222, 10)}
+          strokeColor="#627eea"
+          lineGraphWidth="120"
+        />
+        <LineGraph
+          sampleArray={randomArray(22, 222, 10)}
+          strokeColor="#627eea"
+          lineGraphWidth="120"
+        />
+      </div>
+    </div>
   );
 }

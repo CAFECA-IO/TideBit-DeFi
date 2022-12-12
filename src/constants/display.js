06f34c189400e45a5bcd3dc64ce7d5dae4a8b297
arealclimber <luffulgence@gmail.com>
2022-12-12 18:30:35 +0800

    refactor: 💡 delete config `.js`

--- src/constants/display.js
+++
@@ -1,5 +0,0 @@
-// export const delayedProcessModalDisappear = ({setProcessModalVisible = () => {}}) => {
-//   setTimeout(() => setProcessModalVisible(false), 1000);
-// };
-
-export const DELAYED_HIDDEN_SECONDS = 1000;

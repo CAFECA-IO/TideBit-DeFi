cd77be569b2d89352242f929a31c5e2a7dfe9567
arealclimber <luffulgence@gmail.com>
2022-12-13 19:05:33 +0800

    feat: 🎸 reserve

--- src/components/reserve_ratio/reserve_ratio.tsx
+++ src/components/reserve_ratio/reserve_ratio.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 import Image from 'next/image';
 
 const ReserveRatio = () => {
-  return <div></div>;
+  return (
+    <section className="relative">
+      <div>
+        {/* background image */}
+        <Image
+          className="w-full"
+          src="/elements/group_15244.svg"
+          width={1252}
+          height={879}
+          alt="picture"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default ReserveRatio;

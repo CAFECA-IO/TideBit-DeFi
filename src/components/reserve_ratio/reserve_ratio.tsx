0bd1336b54143fa982f9ca0926af4892f33e05d3
arealclimber <luffulgence@gmail.com>
2022-12-16 10:42:26 +0800

    feat: 🎸 reserve ratio title and crypto title rwd

--- src/components/reserve_ratio/reserve_ratio.tsx
+++ src/components/reserve_ratio/reserve_ratio.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image';
 const ReserveRatio = () => {
   return (
     <section>
-      <div className="mb-40 items-center text-2xl font-medium text-white xs:text-3xl sm:text-4xl">
+      <div className="mb-40 items-center text-2xl font-medium text-white xl:text-4xl">
         <div className="flex items-center justify-center">
           <span className="my-auto h-px w-1/11 rounded bg-white/50 xs:inline-block xs:w-1/10 lg:w-1/5 xl:mx-2"></span>
-          <h1 className="mx-1 text-center xl:w-1/2">
+          <h1 className="mx-1 text-center">
             Latest
             <span className="text-tidebitTheme"> reserve ratio</span> of TideBit holdings
           </h1>

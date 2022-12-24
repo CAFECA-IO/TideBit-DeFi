94e9979fed1fd6df52ee082a8516a9317552780e
arealclimber <luffulgence@gmail.com>
2022-12-24 19:00:59 +0800

    feat: 🎸 trading page order section

--- src/components/position_tab/position_tab.tsx
+++ src/components/position_tab/position_tab.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const TradeTab = () => {
+const PositionTab = () => {
   return (
     <div>
       <div
@@ -13,14 +13,14 @@ const TradeTab = () => {
             <div
               className={`pointer-events-auto ${'w-300px'} h-screen bg-darkGray p-5 text-white transition-all duration-300`}
             >
-              <h1 className="pl-5 text-2xl font-bold">Start to trade</h1>
+              <h1 className="pl-5 text-2xl font-bold">Order information</h1>
 
               {/* <div className="mt-20">
-                <NotificationItem />
-              </div>
-              <div className="mt-5">
-                <NotificationItem />
-              </div> */}
+            <NotificationItem />
+          </div>
+          <div className="mt-5">
+            <NotificationItem />
+          </div> */}
             </div>
           </div>
         </div>
@@ -29,4 +29,4 @@ const TradeTab = () => {
   );
 };
 
-export default TradeTab;
+export default PositionTab;
---
+++ src/components/trade_page_body/trade_page_body.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import OrderSection from '../order_section/order_section';
+
+const TradePageBody = () => {
+  return (
+    <>
+      {/* TODO: MarketSection component */}
+      <OrderSection />
+    </>
+  );
+};
+
+export default TradePageBody;

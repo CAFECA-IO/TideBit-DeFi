e59ccd92b131087a4c1221af733867b28de2090c
arealclimber <luffulgence@gmail.com>
2022-12-29 01:16:09 +0800

    play: 🐸 line graph code on candlestick chart component

--- src/components/market_section/market_section.tsx
+++ src/components/market_section/market_section.tsx
@@ -8,9 +8,14 @@ const MarketSection = () => {
       {/* <div className="pt-700px text-7xl text-blue-100">Market Section</div> */}
       <div className="ml-5 pt-100px">
         <TradingHeader upOrDown="down" />
-        {/* <div className="pt-20">
-          <CandlestickChart />
-        </div> */}
+        <div className="pt-10">
+          <CandlestickChart
+            strokeColor={[`#1AE2A0`]}
+            candlestickChartWidth="700"
+            annotatedValue={1290}
+            candlestickChartHeight="400"
+          />
+        </div>
       </div>
     </div>
   );

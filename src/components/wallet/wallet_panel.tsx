9300649d10f116ce8ae998ae84c04af413e059e1
arealclimber <luffulgence@gmail.com>
2023-01-04 19:36:20 +0800

    feat: 🎸 btn trivial

--- src/components/wallet/wallet_panel.tsx
+++ src/components/wallet/wallet_panel.tsx
@@ -1683,7 +1683,7 @@ export default function WalletPanel({className, getUserLoginState}: IWalletPanel
   ) : (
     <TideButton
       onClick={clickHandler}
-      className={`mt-4 rounded border-0 bg-tidebitTheme py-2 px-5 text-base text-white hover:bg-cyan-600 focus:outline-none md:mt-0`}
+      className={`mt-4 rounded border-0 bg-tidebitTheme py-2 px-5 text-base text-white transition-all hover:opacity-90 md:mt-0`}
     >
       {/* Wallet Connect */}
       {t('nav_bar.WalletConnect')}

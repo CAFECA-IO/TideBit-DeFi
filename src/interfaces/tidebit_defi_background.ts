757c4e09cb2d22adefbecc3c69a5811fc27b6299
arealclimber <luffulgence@gmail.com>
2023-01-11 19:20:23 +0800

    docs: ✏️ interfaces
    ✅ Closes: #114

--- src/interfaces/tidebit_defi_background.ts
+++ src/interfaces/tidebit_defi_background.ts
@@ -1,109 +1,125 @@
-export interface IRealUser {
-  //----- subscribe + 拿到資料 -----
-  id: string;
-  username: string;
-  email?: string;
-  wallet: string[];
-  favoriteTickers: ITickerData[]; // 會拿到哪些是被star，要顯示在favorite tab的
-
-  // 在navbar上的用戶餘額 會定期更新
-  getTotalBalace: () => null;
-  // key: string;
-  // value: number;
-  // Avbl. Margin
-
-  // 在navbar上的PnL 會定期更新
-  getPnL: () => null;
-
-  // 在trade tab這邊的用戶餘額
-  // getBalances;~~
-
-  // 用戶還沒關倉的倉位
-  getOpenedCFD: () => null;
-
-  /*
-   * time: timestamp; 前端拿到該倉位結束的時間，然後自己算出剩下多久
-   * ticker: string;
-   * 操作: string;
-   * 倉位 value: string;
-   * Pnl: number;
-   * 走勢
-   *
-   */
-
-  // 監聽一個user，判斷更新餘額或order狀態
-
-  // 用戶已關倉的倉位
-  getClosedCFD: () => null;
-
-  // My assets
-  getHistory: () => null;
-
-  // 交易對清單要自己比對user跟market
-
-  //----- 拿到資料-----
-  // orderEngine: string; // 產生 EIP 712 / 出金入金 要的資料
+export interface IUser {
+  id: string | null;
+  username: string | null;
+  email: string | null;
+  wallet: string | null;
+  favoriteTickers: ITickerData[];
   isSubscibedNewsletters: boolean;
   isEnabledEmailNotification: boolean;
   isConnected: boolean;
   isConnectedWithEmail: boolean;
   isConnectedWithTideBit: boolean;
-  walletId: string;
-  tideBitId: string;
+  walletId: string | null;
+  tideBitId: string | null;
   enableServiceTerm: boolean;
+  connect: () => Promise<boolean>;
+  disconnect: () => Promise<void>;
+  signServiceTerm: () => Promise<boolean>;
+  addFavoriteTicker: (props: ITickerData) => Promise<void>;
+  removeFavoriteTicker: (props: ITickerData) => Promise<void>;
 
-  //----- 功能 -----
-  // 加追蹤清單
-  addFavoriteTicker: (ticker: string) => void;
-  // 移除追蹤清單
-  removeFavoriteTicker: (ticker: string) => void;
+  balances: IUserBalance[];
+  getTotalBalace: () => IUserBalance[];
+  // getPnL: () => null;
 
-  // 用戶開倉出金入金的功能要的資料格式
-  createOrder: (order: IOrder) => null;
+  CFDs: ICFD[];
+  getOpenedCFD: () => ICFD[];
+  getClosedCFD: () => ICFD[];
 
-  // 用戶關倉的功能要的資料格式
-  closeCFD: (order: IOrder) => null;
-
-  // 用戶調整倉位的功能要的資料格式
-  updateOrder: (order: IOrder) => null;
-
-  // 用戶訂閱電子報
-  subscribeNewsletters: () => null;
+  // getHistory: () => null;
 }
 
-export interface IRealMarket {
-  //----- subscribe + 拿到資料 -----
-  // 下單區的點差
-  // Ticker; // 有整個交易相關的物件資料
+export interface ICFD {
+  // id: string;
+  ticker: string; // 'BTC' | 'ETH'
+  operation: 'buy' | 'sell'; // 'Buy' | 'Sell'
+  // leverage: number;
+  value: number;
+  // pnl: number; // calculated by frontend
+  // trend: number[]; // gained by Market
+  closingTimestamp: number;
+}
 
-  // 交易對清單的線會更新
-  availableTickers: ITickerData[];
+export interface IUserBalance {
+  label: 'available' | 'locked' | 'PNL'; // 'Available' | 'Locked/Margin' | 'PNL'
+  balance: number;
+}
+
+export interface IMarket {
+  // availableTickers: ITickerData[];
+  getTickers: (id: number) => ITickerData[]; // 會拿到交易對清單
+
   isCFDTradable: boolean;
-  getTickers: (ticker: string) => ITickerData; // 會拿到哪些是被star的
-  getTicker: (ticker: string) => ITickerData; // 會拿到現在這個交易對的資料
 
-  // 只有Live會即時更新
-  // 蠟燭圖資料，依照時間間隔，拿到資料，重問一次、重新subscribe，把時間間隔當參數放進去
-  getCandlestickChartData: (ticker: string, interval: string) => ICandlestickChartData;
-
-  // 折線圖資料，依照時間間隔，拿到資料，重問一次、重新subscribe，把時間間隔當參數放進去
-  getTradingChartData: (ticker: string, interval: string) => ITradingChartData;
-
-  //----- 拿到資料-----
+  getTicker: (id: number) => ITickerInfo; // 會拿到現在這個交易對的資料
+  getCandlestickData: (ticker: string, timeSpan: timeSpan) => ICandlestick[];
 }
+
+export type timeSpan = '15s' | '5m' | '15m' | '30m' | '1h' | '4h' | '12h' | '1d';
+
+export interface ICandlestick {
+  timestamp: number;
+  OHLC: number[]; // [open, high, low, close] x 100
+  average: number;
+
+  nowPrice: number;
+
+  latestOHLC: number[]; // [open, high, low, close] x 10
+  // volume: number;
+}
+
+export interface ITickerInfo {
+  label: string;
+  price: number;
+  fluctuating: fluctuatingProps;
+  volume: number; // 24 hr volume
+}
+
+export interface fluctuatingProps {
+  type: 'up' | 'down' | 'equal';
+  value: number;
+  percentage: number;
+}
+
+// export const UserContext = createContext<IUserContext>({
+//   id: null,
+//   username: null,
+//   email: null,
+//   wallet: null,
+//   favoriteTickers: [],
+//   isSubscibedNewsletters: false,
+//   isEnabledEmailNotification: false,
+//   isConnected: false,
+//   isConnectedWithEmail: false,
+//   isConnectedWithTideBit: false,
+//   walletId: null,
+//   tideBitId: null,
+//   enableServiceTerm: false,
+//   connect: () => Promise.resolve(true),
+//   disconnect: () => Promise.resolve(),
+//   signServiceTerm: () => Promise.resolve(true),
+//   addFavoriteTicker: (props: ITickerData) => Promise.resolve(),
+//   removeFavoriteTicker: (props: ITickerData) => Promise.resolve(),
+// });
 
 export interface ITickerData {
-  id: string;
+  currency: string;
+  chain: string;
+  star: boolean;
+  starred: boolean;
+  // starColor: string;
+  // getStarredStateCallback: (bool: boolean) => void;
+  price: number;
+  fluctuating: number;
+  // gradientColor: string;
+  tokenImg: string;
+
+  lineGraphProps: ILineGraphProps;
 }
 
-export interface ICandlestickChartData {
-  id: string;
-}
-
-export interface ITradingChartData {
-  id: string;
-}
-
-export interface IOrder {
-  id: string;
+export interface ILineGraphProps {
+  dataArray?: number[];
+  strokeColor?: string[];
+  lineGraphWidth?: string;
+  lineGraphWidthMobile?: string;
 }

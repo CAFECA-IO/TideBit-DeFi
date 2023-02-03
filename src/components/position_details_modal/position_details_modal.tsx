f73593855181a2485955d15871da49ba63f418dc
arealclimber <luffulgence@gmail.com>
2023-02-03 13:46:19 +0800

    feat: 🎸 zero padding by `.padStart()`
    ✅ Closes: #119

--- src/components/position_details_modal/position_details_modal.tsx
+++ src/components/position_details_modal/position_details_modal.tsx
@@ -20,11 +20,17 @@ const timestampToString = (timestamp: number) => {
 
   const date = new Date(timestamp * 1000);
   const year = date.getFullYear();
-  const month = ('0' + (date.getMonth() + 1)).slice(-2);
-  const day = ('0' + date.getDate()).slice(-2);
-  const hour = ('0' + date.getHours()).slice(-2);
-  const minute = ('0' + date.getMinutes()).slice(-2);
-  const second = ('0' + date.getSeconds()).slice(-2);
+  // const month = ('' + (date.getMonth() + 1)).slice(-2);
+  // const day = ('0' + date.getDate()).slice(-2);
+  // const hour = ('0' + date.getHours()).slice(-2);
+  // const minute = ('0' + date.getMinutes()).slice(-2);
+  // const second = ('0' + date.getSeconds()).slice(-2);
+
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hour = date.getHours().toString().padStart(2, '0');
+  const minute = date.getMinutes().toString().padStart(2, '0');
+  const second = date.getSeconds().toString().padStart(2, '0');
 
   const dateString = `${year}-${month}-${day}`;
   const timeString = `${hour}:${minute}:${second}`;

a481473e759e769f7973250f67253bc5c06d7e7d
arealclimber <luffulgence@gmail.com>
2023-02-03 13:52:16 +0800

    docs: ✏️ comment the updates of IPnL
    ✅ Closes: #181

--- src/interfaces/tidebit_defi_background/pnl.ts
+++ src/interfaces/tidebit_defi_background/pnl.ts
@@ -1,5 +1,13 @@
 export interface IPnL {
   type: 'UP' | 'DOWN' | 'EQUAL';
+  value: string;
+}
+
+/**
+export interface IPnL {
+  type: 'UP' | 'DOWN' | 'EQUAL';
   symbol?: string; // + or -
   value: number;
 }
+
+ */

3356fd736b0f0d23521406e2e07a49c8ab2068de
arealclimber <luffulgence@gmail.com>
2023-02-06 18:56:16 +0800

    feat: 🎸 longPrice and shortPrice and todo recommended tp&sl
    ✅ Closes: #192 #194

--- src/components/trade_tab/trade_tab.tsx
+++ src/components/trade_tab/trade_tab.tsx
@@ -5,16 +5,24 @@ import {AiOutlineQuestionCircle} from 'react-icons/ai';
 import RippleButton from '../ripple_button/ripple_button';
 import {UNIVERSAL_NUMBER_FORMAT_LOCALE} from '../../constants/display';
 
-// TODO: Use Stop loss limit and other data from Market context
 // 1388.4 * 0.82
 const LONG_RESTRICTION_SL = 1138.48;
 // 1388.4 * 1.18
 const SHORT_RESTRICTION_SL = 1638.31;
 
 const TradeTab = () => {
+  // TODO: Use Stop loss limit and other data from Market context
   const MARKET_PRICE = 6290.41;
   const USER_BALANCE = 1000;
   const LEVERAGE = 5;
+  const guranteedStopFee = 0.97;
+  const longPrice = (MARKET_PRICE * 1.18).toFixed(2); // market price * (1+spread)
+  const shortPrice = (MARKET_PRICE * 0.82).toFixed(2); // market price * (1-spread)
+  // TODO: pass and get data from `trading input`
+  const longRecommendedTp = (MARKET_PRICE * 1.2).toFixed(2); // recommendedTp
+  const longRecommendedSl = (MARKET_PRICE * 0.9).toFixed(2);
+  const shortRecommendedTp = (MARKET_PRICE * 0.8).toFixed(2);
+  const shortRecommendedSl = (MARKET_PRICE * 1.1).toFixed(2);
 
   const roundToDecimalPlaces = (val: number, precision: number): number => {
     const roundedNumber = Number(val.toFixed(precision));
@@ -27,7 +35,6 @@ const TradeTab = () => {
 
   const [margingInputValue, setMarginInputValue] = useState(0.02);
 
-  // TODO: useState or constant to refresh?
   const [longTpValue, setLongTpValue] = useState(1388.4);
   const [longSlValue, setLongSlValue] = useState(1328.4);
   const [longTpToggle, setLongTpToggle] = useState(false);
@@ -202,7 +209,7 @@ const TradeTab = () => {
       />
       <label className={`ml-2 flex text-sm font-medium text-lightGray`}>
         Guaranteed stop &nbsp;
-        <span className="text-lightWhite"> (Fee: 0.77 USDT)</span>
+        <span className="text-lightWhite"> (Fee: {guranteedStopFee} USDT)</span>
         {/* <span className="">
           <AiOutlineQuestionCircle size={20} />
         </span> */}
@@ -274,7 +281,7 @@ const TradeTab = () => {
         />
         <label className="ml-2 flex text-sm font-medium text-lightGray">
           Guaranteed stop &nbsp;
-          <span className="text-lightWhite"> (Fee: 0.77 USDT)</span>
+          <span className="text-lightWhite"> (Fee: {guranteedStopFee} USDT)</span>
           {/* <span className="">
           <AiOutlineQuestionCircle size={20} />
         </span> */}
@@ -383,7 +390,7 @@ const TradeTab = () => {
                   className="mr-2 mb-2 rounded-md bg-lightGreen5 px-7 py-1 text-sm font-medium tracking-wide text-white transition-colors duration-300 hover:bg-lightGreen5/80"
                 >
                   <b>UP</b> <br />
-                  Above $ 1545.0
+                  Above $ {longPrice}
                 </RippleButton>
               </div>
 
@@ -446,7 +453,7 @@ const TradeTab = () => {
                     className="mr-2 mb-2 rounded-md bg-lightRed px-7 py-1 text-sm font-medium tracking-wide text-white transition-colors duration-300 hover:bg-lightRed/80"
                   >
                     <b>Down</b> <br />
-                    Below $ 1030.0
+                    Below $ {shortPrice}
                   </RippleButton>
                 </div>
               </div>

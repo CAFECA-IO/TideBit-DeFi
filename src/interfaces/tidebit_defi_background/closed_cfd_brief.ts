a03784e120c4506eefffe7f1f5aff51c332c3c41
Julian <julian890701@gmail.com>
2023-02-08 14:59:20 +0800

    Set dummy data

--- src/interfaces/tidebit_defi_background/closed_cfd_brief.ts
+++ src/interfaces/tidebit_defi_background/closed_cfd_brief.ts
@@ -4,3 +4,19 @@ export interface IClosedCFDBrief extends ICFDBrief {
   closedTimestamp: number;
   closedValue: number;
 }
+
+export const dummyCloseCFDBrief: IClosedCFDBrief = {
+  id: 'TBD202302080000001',
+  ticker: 'ETH',
+  typeOfPosition: 'SELL',
+  openPrice: 37.8,
+  openValue: 739.2,
+  openTimestamp: 12,
+  closedTimestamp: 9,
+  closedValue: 824.3,
+  pNL: {
+    type: 'DOWN',
+    symbol: '-',
+    value: 90204,
+  },
+};
--- src/interfaces/tidebit_defi_background/open_cfd_brief.ts
+++ src/interfaces/tidebit_defi_background/open_cfd_brief.ts
@@ -1,6 +1,22 @@
+import PositionLineGraph from '../../components/position_line_graph/position_line_graph';
 import {ICFDBrief} from './cfd_brief';
 import {ITickerLineGraph} from './ticker_line_graph';
 
 export interface IOpenCFDBrief extends ICFDBrief {
   positionLineGraph: ITickerLineGraph;
 }
+
+export const dummyOpenCFDBrief: IOpenCFDBrief = {
+  id: 'TBD202302080000001',
+  ticker: 'BTC',
+  typeOfPosition: 'BUY',
+  openPrice: 24.5,
+  openValue: 656.9,
+  openTimestamp: 14,
+  pNL: {
+    type: 'UP',
+    symbol: '+',
+    value: 90752,
+  },
+  positionLineGraph: {dataArray: [153000, 137200, 122000, 126500, 134200, 129900]},
+};

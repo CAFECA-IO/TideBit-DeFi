55175e77bc07a823bad26cf7901ed1b2cc013aae
arealclimber <luffulgence@gmail.com>
2023-02-21 15:27:26 +0800

    feat: 🎸 detect the change on `PositionDetailsModal` immediately
    ✅ Closes: #119

--- src/components/position_details_modal/position_details_modal.tsx
+++ src/components/position_details_modal/position_details_modal.tsx
@@ -7,7 +7,8 @@ import {
   UNIVERSAL_NUMBER_FORMAT_LOCALE,
 } from '../../constants/display';
 import Toggle from '../toggle/toggle';
-import {useContext, useRef, useState} from 'react';
+import {useContext, useEffect, useRef} from 'react';
+
 import TradingInput from '../trading_input/trading_input';
 import {AiOutlineQuestionCircle} from 'react-icons/ai';
 import RippleButton from '../ripple_button/ripple_button';
@@ -16,6 +17,7 @@ import {TypeOfPosition} from '../../constants/type_of_position';
 import {ProfitState} from '../../constants/profit_state';
 import {timestampToString} from '../../lib/common';
 import {MarketContext} from '../../contexts/market_context';
+import useState from 'react-usestateref';
 
 interface IPositionDetailsModal {
   modalVisible: boolean;
@@ -33,43 +35,111 @@ const PositionDetailsModal = ({
   ...otherProps
 }: IPositionDetailsModal) => {
   // console.log('openCfdDetails in details modal: ', openCfdDetails.id);
-  const globalContext = useGlobal();
+  const globalCtx = useGlobal();
   const marketCtx = useContext(MarketContext);
 
   const initialTpToggle = openCfdDetails?.takeProfit ? true : false;
   const initialSlToggle = openCfdDetails?.stopLoss ? true : false;
 
-  const initialSlInput = openCfdDetails?.stopLoss ?? openCfdDetails?.recommendedSl ?? 0;
-  const initialTpInput = openCfdDetails?.takeProfit ?? openCfdDetails?.recommendedTp ?? 0;
+  const initialSlInput = openCfdDetails?.stopLoss ?? openCfdDetails.recommendedSl;
+  const initialTpInput = openCfdDetails?.takeProfit ?? openCfdDetails.recommendedTp;
 
-  const [takeProfitValue, setTakeProfitValue] = useState(initialTpInput);
-  const [stopLossValue, setStopLossValue] = useState(initialSlInput);
-  const [takeProfitToggle, setTakeProfitToggle] = useState(initialTpToggle);
-  const [stopLossToggle, setStopLossToggle] = useState(initialSlToggle);
+  const initialGuaranteedChecked = openCfdDetails.guaranteedStop;
+
+  const [tpValue, setTpValue, tpValueRef] = useState(initialTpInput);
+  const [slValue, setSlValue, slValueRef] = useState(initialSlInput);
+  const [tpToggle, setTpToggle, tpToggleRef] = useState(initialTpToggle);
+  const [slToggle, setSlToggle, slToggleRef] = useState(initialSlToggle);
+  const [guaranteedChecked, setGuaranteedChecked, guaranteedStopCheckRef] =
+    useState(initialGuaranteedChecked);
+
   const [guaranteedTooltipStatus, setGuaranteedTooltipStatus] = useState(0);
 
-  const [guaranteedChecked, setGuaranteedChecked] = useState(openCfdDetails.guaranteedStop);
   const [slLowerLimit, setSlLowerLimit] = useState(0);
   const [slUpperLimit, setSlUpperLimit] = useState(Infinity);
 
   const [submitDisabled, setSubmitDisabled] = useState(true);
 
+  const disabledButton =
+    initialSlToggle === slToggle ||
+    initialTpToggle === tpToggle ||
+    (initialSlToggle && initialSlInput === slValue) ||
+    (initialTpToggle && initialTpInput === tpValue) ||
+    initialGuaranteedChecked === guaranteedChecked;
+
   const getToggledTpSetting = (bool: boolean) => {
-    setTakeProfitToggle(bool);
+    // setSubmitDisabled(true);
+
+    setTpToggle(bool);
+
+    // if (bool !== initialTpToggle) {
+    //   setSubmitDisabled(false);
+    // }
   };
 
   const getToggledSlSetting = (bool: boolean) => {
-    setStopLossToggle(bool);
+    // setSubmitDisabled(true);
+
+    setSlToggle(bool);
+
+    // if (bool !== initialTpToggle) {
+    //   setSubmitDisabled(false);
+    // }
   };
 
   const getTpValue = (value: number) => {
-    setTakeProfitValue(value);
+    // setSubmitDisabled(true);
+
+    setTpValue(value);
+
+    // if (value !== initialTpInput) {
+    //   setSubmitDisabled(false);
+    // }
     // console.log('tp value from Trading Input:', value);
   };
 
   const getSlValue = (value: number) => {
-    setStopLossValue(value);
+    // setSubmitDisabled(true);
+
+    setSlValue(value);
+
+    // if (value !== initialSlInput) {
+    //   setSubmitDisabled(false);
+    // }
     // console.log('sl value from Trading Input:', value);
+  };
+
+  const guaranteedCheckedChangeHandler = () => {
+    // If position is not guaranteed, then set the stop loss to the recommended value
+    // setSubmitDisabled(true);
+
+    // console.log('guaranteedChecked: ', guaranteedStopCheckRef.current);
+
+    if (!openCfdDetails?.guaranteedStop) {
+      // if (openCfdDetails.guaranteedStop) {
+      //   setSubmitDisabled(true);
+      //   return;
+      // }
+
+      setGuaranteedChecked(!guaranteedChecked);
+      setSlToggle(true);
+      setSlLowerLimit(0);
+      setSlUpperLimit(Infinity);
+
+      // setSubmitDisabled(false);
+
+      return;
+    } else {
+      setSlLowerLimit(openCfdDetails?.stopLoss ?? openCfdDetails?.recommendedSl);
+      setSlUpperLimit(openCfdDetails?.stopLoss ?? openCfdDetails?.recommendedSl);
+      setSlValue(openCfdDetails?.stopLoss ?? openCfdDetails?.recommendedSl);
+
+      // setSubmitDisabled(true);s
+
+      return;
+    }
+
+    // if (openCfdDetails)
   };
 
   // const getSlToggleFunction = (slToggleFunction: () => void) => {
@@ -102,8 +172,8 @@ const PositionDetailsModal = ({
       ? TypeOfBorderColor.LONG
       : TypeOfBorderColor.SHORT;
 
-  const isDisplayedTakeProfitSetting = takeProfitToggle ? 'flex' : 'invisible';
-  const isDisplayedStopLossSetting = stopLossToggle ? 'flex' : 'invisible';
+  const isDisplayedTakeProfitSetting = tpToggle ? 'flex' : 'invisible';
+  const isDisplayedStopLossSetting = slToggle ? 'flex' : 'invisible';
 
   const displayedSlLowerLimit = openCfdDetails?.guaranteedStop
     ? openCfdDetails?.stopLoss ?? openCfdDetails.recommendedSl
@@ -121,37 +191,37 @@ const PositionDetailsModal = ({
     let changedProperties = {};
 
     // Detect if tpValue has changed
-    if (takeProfitToggle && takeProfitValue !== openCfdDetails.takeProfit) {
+    if (tpToggle && tpValue !== openCfdDetails.takeProfit) {
       changedProperties = {
         ...changedProperties,
-        takeProfitAmount: takeProfitValue,
+        takeProfitAmount: tpValue,
       };
     }
 
     // Detect if spValue has changed
-    if (stopLossToggle && stopLossValue !== openCfdDetails.stopLoss) {
-      changedProperties = {...changedProperties, stopLossAmount: stopLossValue};
+    if (slToggle && slValue !== openCfdDetails.stopLoss) {
+      changedProperties = {...changedProperties, stopLossAmount: slValue};
     }
 
     // Detect if tpToggle has changed
-    if (initialTpToggle !== takeProfitToggle) {
+    if (initialTpToggle !== tpToggle) {
       changedProperties = {
         ...changedProperties,
-        takeProfitAmount: takeProfitToggle ? takeProfitValue : 0,
+        takeProfitAmount: tpToggle ? tpValue : 0,
       };
     }
 
     // Detect if slToggle has changed
-    if (initialSlToggle !== stopLossToggle) {
+    if (initialSlToggle !== slToggle) {
       changedProperties = {
         ...changedProperties,
-        stopLossAmount: stopLossToggle ? stopLossValue : 0,
+        stopLossAmount: slToggle ? slValue : 0,
       };
     }
 
     // Detect if guaranteedStop has changed
     if (guaranteedChecked !== openCfdDetails.guaranteedStop) {
-      const stopLossAmount = stopLossValue !== openCfdDetails.stopLoss ? stopLossValue : undefined;
+      const stopLossAmount = slValue !== openCfdDetails.stopLoss ? slValue : undefined;
       changedProperties = {
         ...changedProperties,
         guaranteedStopChecked: guaranteedChecked,
@@ -161,12 +231,12 @@ const PositionDetailsModal = ({
 
     // If there's no updates, do nothing
     if (Object.keys(changedProperties).length > 0) {
-      setSubmitDisabled(false);
+      globalCtx.visiblePositionDetailsModalHandler();
 
       // TODO: send changedProperties to MetaMask for signature
       changedProperties = {orderId: openCfdDetails.id, ...changedProperties};
 
-      globalContext.toast({
+      globalCtx.toast({
         type: 'info',
         message: 'Changes: \n' + JSON.stringify(changedProperties),
         toastId: JSON.stringify(changedProperties),
@@ -189,14 +259,42 @@ const PositionDetailsModal = ({
     }
   };
 
+  useEffect(() => {
+    setSubmitDisabled(true);
+    if (tpToggleRef.current && tpValueRef.current !== openCfdDetails?.takeProfit) {
+      setSubmitDisabled(false);
+      // console.log('tpValueRef current', tpValueRef.current);
+    }
+
+    if (slToggleRef.current && slValueRef.current !== openCfdDetails?.stopLoss) {
+      setSubmitDisabled(false);
+      // console.log('slValueRef current', slValueRef.current);
+    }
+
+    if (tpToggleRef.current !== initialTpToggle) {
+      setSubmitDisabled(false);
+      // console.log('tpToggleRef current', tpToggleRef.current);
+    }
+
+    if (slToggleRef.current !== initialSlToggle) {
+      setSubmitDisabled(false);
+      // console.log('slToggleRef current', slToggleRef.current);
+    }
+
+    if (guaranteedStopCheckRef.current !== openCfdDetails.guaranteedStop) {
+      setSubmitDisabled(false);
+      // console.log('guaranteedStopCheckRef current', guaranteedStopCheckRef.current);
+    }
+  }, [tpValueRef.current, slValueRef.current, tpToggleRef.current, slToggleRef.current]);
+
   const displayedTakeProfitSetting = (
     <div className={`${isDisplayedTakeProfitSetting}`}>
       <TradingInput
         getInputValue={getTpValue}
         lowerLimit={0}
-        inputInitialValue={takeProfitValue}
-        inputValueFromParent={takeProfitValue}
-        setInputValueFromParent={setTakeProfitValue}
+        inputInitialValue={tpValue}
+        inputValueFromParent={tpValue}
+        setInputValueFromParent={setTpValue}
         inputPlaceholder="take profit"
         inputName="tpInput"
         inputSize="h-25px w-70px text-sm"
@@ -212,9 +310,9 @@ const PositionDetailsModal = ({
         getInputValue={getSlValue}
         lowerLimit={displayedSlLowerLimit}
         upperLimit={displayedSlUpperLimit}
-        inputInitialValue={stopLossValue}
-        setInputValueFromParent={setStopLossValue}
-        inputValueFromParent={stopLossValue}
+        inputInitialValue={slValue}
+        setInputValueFromParent={setSlValue}
+        inputValueFromParent={slValue}
         inputPlaceholder="stop loss"
         inputName="slInput"
         inputSize="h-25px w-70px text-sm"
@@ -223,19 +321,6 @@ const PositionDetailsModal = ({
       />
     </div>
   );
-  const guaranteedCheckedChangeHandler = () => {
-    if (!openCfdDetails?.guaranteedStop) {
-      setGuaranteedChecked(!guaranteedChecked);
-      setStopLossToggle(true);
-      setSlLowerLimit(0);
-      setSlUpperLimit(Infinity);
-      setSubmitDisabled(false);
-    } else {
-      setSlLowerLimit(openCfdDetails?.stopLoss ?? openCfdDetails?.recommendedSl);
-      setSlUpperLimit(openCfdDetails?.stopLoss ?? openCfdDetails?.recommendedSl);
-      setStopLossValue(openCfdDetails?.stopLoss ?? openCfdDetails?.recommendedSl);
-    }
-  };
 
   const guaranteedStopLoss = (
     <div className="">
@@ -411,8 +496,8 @@ const PositionDetailsModal = ({
                   <div className="text-lightGray">Close at profit</div>
                   <div className="-mr-10">{displayedTakeProfitSetting}</div>
                   <Toggle
-                    setToggleStateFromParent={setTakeProfitToggle}
-                    toggleStateFromParent={takeProfitToggle}
+                    setToggleStateFromParent={setTpToggle}
+                    toggleStateFromParent={tpToggle}
                     getToggledState={getToggledTpSetting}
                   />
                 </div>
@@ -424,19 +509,20 @@ const PositionDetailsModal = ({
                     getToggledState={getToggledSlSetting}
                     lockedToOpen={guaranteedChecked}
                     initialToggleState={guaranteedChecked}
-                    toggleStateFromParent={stopLossToggle}
-                    setToggleStateFromParent={setStopLossToggle}
+                    toggleStateFromParent={slToggle}
+                    setToggleStateFromParent={setSlToggle}
                     // getToggleFunction={getSlToggleFunction}
                   />
                 </div>
 
                 {guaranteedStopLoss}
 
+                {/* TODO: T/P value changed, S/L value changed, guaranteed-stop check changed, T/P toggle changed, S/L toggle changed */}
                 <RippleButton
                   disabled={submitDisabled}
                   onClick={buttonClickHandler}
                   buttonType="button"
-                  className="mt-5 rounded border-0 bg-tidebitTheme px-32 py-2 text-base text-white transition-colors duration-300 hover:cursor-pointer hover:bg-cyan-600 focus:outline-none md:mt-0"
+                  className="mt-5 rounded border-0 bg-tidebitTheme px-32 py-2 text-base text-white transition-colors duration-300 hover:cursor-pointer hover:bg-cyan-600 focus:outline-none disabled:bg-lightGray md:mt-0"
                 >
                   Update Position
                 </RippleButton>

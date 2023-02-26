fed3d14a1ef0b987805c75547e694d2e7aee51ba
arealclimber <luffulgence@gmail.com>
2023-02-27 00:39:49 +0800

    feat: 🎸 balance section functionality

--- src/components/balance_section/balance_section.tsx
+++ src/components/balance_section/balance_section.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CircularProgressBar from '../circular_progress_bar/circular_progress_bar';
 import Image from 'next/image';
 
 const BalanceSection = () => {
   // TODO: userBalance from userContext
-  const totalBalance = 200;
-  const avblBalance = 80;
+  // TODO: fixed to two decimal places
+  const totalBalance = Number((200.8489).toFixed(2));
+  const avblBalance = Number((80.5233).toFixed(2));
+  const lockedBalance = Number((120.3256).toFixed(2));
+
+  const [hidden, setHidden] = useState(false);
 
   const circleSize = '380';
   const eyeIconSize = 30;
 
+  const hiddenClickHandler = () => {
+    setHidden(!hidden);
+  };
+
+  const displayedIcon = hidden ? (
+    <Image
+      src="/elements/group_152051.svg"
+      width={eyeIconSize}
+      height={eyeIconSize}
+      alt="eye close icon"
+    />
+  ) : (
+    <Image
+      src="/elements/group_15205.svg"
+      width={eyeIconSize}
+      height={eyeIconSize}
+      alt="eye open icon"
+    />
+  );
+
+  const displayedBalance = hidden ? '********' : totalBalance;
+
+  const displayedAvblBalance = hidden ? '*****' : avblBalance;
+
+  const displayedLockedBalance = hidden ? '*****' : lockedBalance;
+
   return (
     <div className="">
       <div className="">
-        <div className="imgContainer">
+        <div className="balanceImgContainer">
           <CircularProgressBar
             progressBarColor={['#29C1E1']}
             numerator={avblBalance}
@@ -23,28 +53,23 @@ const BalanceSection = () => {
           />
         </div>
 
-        <div className="textCentered dark:bg-cuteBlue">
-          <div className="flex items-center space-x-2">
-            <p>Total Balance</p>{' '}
-            <button type="button" className="hover:cursor-pointer">
-              <Image
-                src="/elements/group_15205.svg"
-                width={eyeIconSize}
-                height={eyeIconSize}
-                alt="eye open icon"
-              />
-
-              <Image
-                src="/elements/group_152051.svg"
-                width={eyeIconSize}
-                height={eyeIconSize}
-                alt="eye close icon"
-              />
+        <div className="balanceTextCentered space-y-6 dark:bg-black">
+          <div className="flex items-center justify-center space-x-2 text-center">
+            <p className="text-base text-lightGray">Total Balance</p>{' '}
+            <button onClick={hiddenClickHandler} type="button" className="hover:cursor-pointer">
+              {displayedIcon}
             </button>
           </div>
-          <div className="">226283 USDT</div>
+
+          <div className="flex justify-center text-3xl font-extrabold">
+            {displayedBalance}&nbsp;&nbsp;USDT
+          </div>
+
           <div className="">
-            <div className="">Avbl: 100000 / Locked: 126283</div>
+            <div className="text-xs text-lightGray">
+              Avbl: <span className="text-base text-lightWhite">{displayedAvblBalance}</span> /
+              Locked: <span className="text-base text-lightWhite">{displayedLockedBalance}</span>
+            </div>
             <div className=""></div>
           </div>
         </div>

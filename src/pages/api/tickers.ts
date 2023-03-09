939e55ae3d4e912328413008c62c9f0833219ecf
TzuHanLiang <crocodilestear@gmail.com>
2023-03-09 11:35:31 +0800

    feat: #331

--- src/pages/api/tickers.ts
+++ src/pages/api/tickers.ts
@@ -2,5 +2,5 @@ import {NextApiRequest, NextApiResponse} from 'next';
 import {dummyTickers} from '../../interfaces/tidebit_defi_background/ticker_data';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json([dummyTickers]);
+  res.status(200).json([...dummyTickers]);
 }

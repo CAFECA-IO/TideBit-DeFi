a836aaf1d2ec99fb7c6550861d1904f7576ce95b
arealclimber <luffulgence@gmail.com>
2023-04-20 13:59:55 +0800

    fix: 🐛 PNL of open position item with quotation fetched
    ✅ Closes: #525

--- src/components/open_sub_tab/open_sub_tab.tsx
+++ src/components/open_sub_tab/open_sub_tab.tsx
@@ -1,23 +1,205 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import OpenPositionItem from '../open_position_item/open_position_item';
 import {UserContext} from '../../contexts/user_context';
 import {MarketContext} from '../../contexts/market_context';
-import {toDisplayCFDOrder} from '../../lib/common';
+import {getTimestamp, getTimestampInMilliseconds, toDisplayCFDOrder} from '../../lib/common';
 import {
   IDisplayCFDOrder,
   listDummyDisplayCFDOrder,
 } from '../../interfaces/tidebit_defi_background/display_accepted_cfd_order';
+import {TypeOfPosition} from '../../constants/type_of_position';
+import {
+  DISPLAY_QUOTATION_RENEWAL_INTERVAL_SECONDS,
+  QUOTATION_RENEWAL_INTERVAL_SECONDS,
+  WAITING_TIME_FOR_USER_SIGNING,
+  unitAsset,
+} from '../../constants/config';
+import {IQuotation, getDummyQuotation} from '../../interfaces/tidebit_defi_background/quotation';
+import {defaultResultSuccess} from '../../interfaces/tidebit_defi_background/result';
+import useStateRef from 'react-usestateref';
+
+const DEFAULT_TICKER = 'ETH';
+const SELL_PRICE_ERROR = 0;
+const BUY_PRICE_ERROR = 0;
 
 const OpenSubTab = () => {
   const {openCFDs} = useContext(UserContext);
   const marketCtx = useContext(MarketContext);
+  const ticker = marketCtx?.selectedTickerRef?.current?.currency ?? DEFAULT_TICKER;
+
+  const defaultBuyQuotation: IQuotation = getDummyQuotation(ticker, TypeOfPosition.BUY);
+  const defaultSellQuotation: IQuotation = getDummyQuotation(ticker, TypeOfPosition.SELL);
+
+  const [longQuotation, setLongQuotation, longQuotationRef] =
+    useStateRef<IQuotation>(defaultBuyQuotation);
+  const [shortQuotation, setShortQuotation, shortQuotationRef] =
+    useStateRef<IQuotation>(defaultSellQuotation);
+
+  const [secondsLeft, setSecondsLeft, secondsLeftRef] = useStateRef(
+    DISPLAY_QUOTATION_RENEWAL_INTERVAL_SECONDS
+  );
+
+  const getQuotation = async (tickerId: string) => {
+    let longQuotation = defaultResultSuccess;
+    let shortQuotation = defaultResultSuccess;
+
+    try {
+      longQuotation = await marketCtx.getCFDQuotation(tickerId, TypeOfPosition.BUY);
+      shortQuotation = await marketCtx.getCFDQuotation(tickerId, TypeOfPosition.SELL);
+
+      const long = longQuotation.data as IQuotation;
+      const short = shortQuotation.data as IQuotation;
+
+      // Info: if there's error fetching quotation, use the previous quotation or calculate the quotation (20230327 - Shirley)
+      if (
+        longQuotation.success &&
+        long.typeOfPosition === TypeOfPosition.BUY &&
+        longQuotation.data !== null
+      ) {
+        setLongQuotation(long);
+      } else {
+        const buyPrice =
+          (marketCtx.selectedTickerRef.current?.price ?? BUY_PRICE_ERROR) *
+          (1 + (marketCtx.tickerLiveStatistics?.spread ?? 0));
+
+        const buyQuotation: IQuotation = {
+          ticker: marketCtx.selectedTicker?.currency ?? DEFAULT_TICKER,
+          targetAsset: marketCtx.selectedTicker?.currency ?? DEFAULT_TICKER,
+          typeOfPosition: TypeOfPosition.BUY,
+          unitAsset: unitAsset,
+          price: buyPrice,
+          deadline: getTimestamp() + QUOTATION_RENEWAL_INTERVAL_SECONDS,
+          signature: '0x',
+        };
+
+        setLongQuotation(buyQuotation);
+      }
+
+      // Info: if there's error fetching quotation, use the previous quotation or calculate the quotation (20230327 - Shirley)
+      if (
+        shortQuotation.success &&
+        short &&
+        short.typeOfPosition === TypeOfPosition.SELL &&
+        shortQuotation.data !== null
+      ) {
+        setShortQuotation(short);
+      } else {
+        const sellPrice =
+          (marketCtx.selectedTickerRef.current?.price ?? BUY_PRICE_ERROR) *
+          (1 + (marketCtx.tickerLiveStatistics?.spread ?? 0));
+
+        const sellQuotation: IQuotation = {
+          ticker: marketCtx.selectedTicker?.currency ?? DEFAULT_TICKER,
+          targetAsset: marketCtx.selectedTicker?.currency ?? DEFAULT_TICKER,
+          typeOfPosition: TypeOfPosition.SELL,
+          unitAsset: unitAsset,
+          price: sellPrice,
+          deadline: getTimestamp() + QUOTATION_RENEWAL_INTERVAL_SECONDS,
+          signature: '0x',
+        };
+
+        setShortQuotation(sellQuotation);
+      }
+    } catch (err) {
+      const buyPrice =
+        (marketCtx.selectedTickerRef.current?.price ?? BUY_PRICE_ERROR) *
+        (1 + (marketCtx.tickerLiveStatistics?.spread ?? 0));
+
+      const buyQuotation: IQuotation = {
+        ticker: marketCtx.selectedTicker?.currency ?? DEFAULT_TICKER,
+        targetAsset: marketCtx.selectedTicker?.currency ?? DEFAULT_TICKER,
+        typeOfPosition: TypeOfPosition.BUY,
+        unitAsset: unitAsset,
+        price: buyPrice,
+        deadline: getTimestamp() + QUOTATION_RENEWAL_INTERVAL_SECONDS,
+        signature: '0x',
+      };
+
+      setLongQuotation(buyQuotation);
+
+      const sellPrice =
+        (marketCtx.selectedTickerRef.current?.price ?? SELL_PRICE_ERROR) *
+        (1 - (marketCtx.tickerLiveStatistics?.spread ?? 0));
+
+      const sellQuotation: IQuotation = {
+        ticker: marketCtx.selectedTicker?.currency ?? DEFAULT_TICKER,
+        targetAsset: marketCtx.selectedTicker?.currency ?? DEFAULT_TICKER,
+        typeOfPosition: TypeOfPosition.SELL,
+        unitAsset: unitAsset,
+        price: sellPrice,
+        deadline: getTimestamp() + QUOTATION_RENEWAL_INTERVAL_SECONDS,
+        signature: '0x',
+      };
+
+      setShortQuotation(sellQuotation);
+    }
+
+    return {
+      longQuotation: longQuotation.data as IQuotation,
+      shortQuotation: shortQuotation.data as IQuotation,
+    };
+  };
+
+  const setQuotation = async () => {
+    await getQuotation(marketCtx.selectedTicker?.currency ?? DEFAULT_TICKER);
+
+    // Deprecated: (20230420 - Shirley)
+    // eslint-disable-next-line no-console
+    console.log('longQuotationRef.current', longQuotationRef.current);
+    // eslint-disable-next-line no-console
+    console.log('shortQuotationRef.current', shortQuotationRef.current);
+  };
+
+  useEffect(() => {
+    return () => {
+      setQuotation();
+    };
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(async () => {
+      if (!longQuotationRef.current || !shortQuotationRef.current) return;
+
+      const base = longQuotationRef.current.deadline - WAITING_TIME_FOR_USER_SIGNING;
+      const tickingSec = (base * 1000 - getTimestampInMilliseconds()) / 1000;
+      setSecondsLeft(tickingSec > 0 ? Math.round(tickingSec) : 0);
+
+      if (secondsLeftRef.current === 0) {
+        setQuotation();
+      }
+
+      // Deprecated: (20230420 - Shirley)
+      // eslint-disable-next-line no-console
+      console.log('secondsLeftRef.current', secondsLeftRef.current);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [secondsLeft]);
 
   const cfds = openCFDs
     .map(cfd => {
       const positionLineGraph = marketCtx.listTickerPositions(cfd.targetAsset, {
         begin: cfd.createTimestamp,
       });
-      const displayCFD: IDisplayCFDOrder = toDisplayCFDOrder(cfd, positionLineGraph);
+      const quotation =
+        cfd.typeOfPosition === TypeOfPosition.BUY
+          ? longQuotationRef.current
+          : shortQuotationRef.current;
+
+      const displayCFD: IDisplayCFDOrder = toDisplayCFDOrder(cfd, positionLineGraph, quotation);
+
+      // Deprecated: (20230420 - Shirley)
+      // eslint-disable-next-line no-console
+      console.log('line graph', positionLineGraph);
+      // eslint-disable-next-line no-console
+      console.log('open price', displayCFD.openPrice);
+      // eslint-disable-next-line no-console
+      console.log('quotation price', quotation.price);
+      // eslint-disable-next-line no-console
+      console.log('pnl', displayCFD.pnl);
+
       return displayCFD;
     })
     .sort((a, b) => {
@@ -37,10 +219,6 @@ const OpenSubTab = () => {
   //   .sort((a, b) => {
   //     return b.stateCode - a.stateCode;
   //   });
-
-  // Deprecated: [debug] (20230413 - Shirley)
-  // eslint-disable-next-line no-console
-  console.log('cfd from ctx ', JSON.parse(JSON.stringify(cfds)));
 
   const openPositionList = cfds.map(cfd => {
     return (

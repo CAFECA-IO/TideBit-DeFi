1929ce274734126af0c14f8229b2aa851f9ad61d
arealclimber <luffulgence@gmail.com>
2023-05-08 11:14:32 +0800

    play: 🐸 `flex-end` make it center in some circumstances

--- src/pages/api/images/cfd.tsx
+++ src/pages/api/images/cfd.tsx
@@ -14,6 +14,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   // TODO: get icon url from api (20230503 - Shirley)
   const iconUrl = 'https://svgshare.com/i/spv.svg';
   const randomPnL = 16;
+  const user = 'Z';
+  const targetAssetName = 'Ethereum';
+  const typeOfPosition = 'Up (Buy)';
+  const openPrice = 1313.8;
+  const closePrice = 1383.6;
+  const leverage = 5;
+
   // const generateQRCode = async (text: string) => {
   //   try {
   //     const qrcode = await QRCode.toDataURL(text, {
@@ -40,77 +47,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         style={{
           display: 'flex',
           flexDirection: 'column',
+          fontFamily: 'barlow',
         }}
       >
-        {/* <style jsx>{`
-          .fixed {
-            position: fixed;
-          }
-
-          .inset0 {
-            top: 0;
-            right: 0;
-            bottom: 0;
-            left: 0;
-          }
-
-          .z50 {
-            z-index: 50;
-          }
-
-          .flex {
-            display: flex;
-          }
-
-          .itemsCenter {
-            align-items: center;
-          }
-
-          .justifyCenter {
-            justify-content: center;
-          }
-
-          .overflowYAuto {
-            overflow-y: auto;
-          }
-
-          .overflowXHidden {
-            overflow-x: hidden;
-          }
-
-          .outlineNone {
-            outline: none;
-          }
-
-          .backdropBlurSm {
-            backdrop-filter: blur(4px);
-          }
-
-          .focusOutlineNone:focus {
-            outline: none;
-          }
-
-          .z40 {
-            z-index: 40;
-          }
-
-          .bgBlack {
-            background-color: #000000;
-          }
-
-          .bgBlue {
-            background-color: #74cffd;
-          }
-
-          .opacity25 {
-            opacity: 0.25;
-          }
-
-          // ... remaining CSS classes ...
-        `}</style> */}
         <div
           style={{
-            zIndex: -1,
+            // zIndex: -1,
             // TODO: image should upload to server and fix that image URL (20230505 - Shirley)
             backgroundImage: `url('https://gcdnb.pbrd.co/images/WkqDgGonPnnp.png?o=1')`,
             backgroundSize: '600px 600px',
@@ -124,16 +66,33 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             justifyContent: 'flex-start',
             height: '600px',
             width: '600px',
+            fontFamily: 'barlow',
           }}
         >
-          <div style={{marginLeft: 64, marginTop: 10, display: 'flex', alignItems: 'center'}}>
+          <div
+            style={{
+              marginLeft: 64,
+              marginTop: 10,
+              display: 'flex',
+              alignItems: 'center',
+              fontFamily: 'barlow',
+            }}
+          >
             <div
-              style={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '1rem'}}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                gap: '1rem',
+                fontFamily: 'barlow',
+              }}
             >
               <img src={`${iconUrl}`} width={45} height={45} alt="asset icon" />
-              <h1 style={{fontSize: 36, fontWeight: 'normal', color: '#fff'}}>Ethereum</h1>
+              <h1 style={{fontSize: 36, fontWeight: 'normal', color: '#fff', fontFamily: 'barlow'}}>
+                {targetAssetName}
+              </h1>
             </div>
-            <div style={{marginTop: 10, display: 'flex'}}>
+            <div style={{marginTop: 10, display: 'flex', fontFamily: 'barlow'}}>
               <p
                 style={{
                   marginTop: '0.5rem',
@@ -144,33 +103,45 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                   fontSize: '0.875rem',
                   fontWeight: 'bold',
                   color: 'black',
+                  fontFamily: 'barlow',
                 }}
               >
-                Up (Buy)
+                {typeOfPosition}
               </p>
             </div>
           </div>{' '}
           <div
             style={{
               display: 'flex',
+              fontFamily: 'barlow',
             }}
           ></div>
-          <div style={{display: 'flex', marginLeft: '4rem', marginTop: '3rem'}}>
+          <div
+            style={{display: 'flex', marginLeft: '4rem', marginTop: '3rem', fontFamily: 'barlow'}}
+          >
             <div
-              style={{display: 'flex', marginLeft: '3.5rem', marginTop: '1.25rem', width: '100%'}}
+              style={{
+                display: 'flex',
+                marginLeft: '3.5rem',
+                // marginTop: '1.25rem',
+                width: '100%',
+                fontFamily: 'barlow',
+              }}
             >
               <div
                 style={{
                   display: 'flex',
                   marginLeft: '0',
-                  width: '22.5rem',
+                  width: '350px',
+                  height: '320px',
                   borderWidth: '1px',
                   borderColor: TypeOfPnLColorHex.PROFIT,
                   paddingTop: '1rem',
                   paddingBottom: '1rem',
-                  fontSize: '0.75rem',
+                  fontSize: '16px',
                   lineHeight: '1.5',
                   color: 'rgba(229, 231, 235, 1)',
+                  fontFamily: 'barlow',
                 }}
               >
                 <div
@@ -179,12 +150,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     flexDirection: 'column',
                     justifyContent: 'center',
                     textAlign: 'center',
+                    fontFamily: 'barlow',
                   }}
                 >
                   <div
                     style={{
                       position: 'relative',
-                      margin: 'auto',
+                      // margin: 'auto',
+                      marginLeft: '115px',
                       display: 'flex', // inline-flex // FIXME: inline-flex is not working
                       height: '7rem',
                       width: '7rem',
@@ -194,31 +167,136 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                       borderRadius: '50%',
                       backgroundColor: TypeOfPnLColorHex.TIDEBIT_THEME,
                       color: 'rgba(229, 231, 235, 1)',
+                      fontFamily: 'barlow',
                     }}
                   >
                     <span
                       style={{
                         fontSize: 48,
-                        fontWeight: 'bold',
+                        fontWeight: 'extrabold',
                         color: 'rgba(229, 231, 235, 1)',
+                        fontFamily: 'barlow',
                       }}
                     >
-                      Z
+                      {user}
                     </span>
                   </div>{' '}
-                  <p
+                  <div style={{display: 'flex', marginLeft: '100px'}}>
+                    <span style={{marginTop: '18px'}}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="28.125"
+                        height="36"
+                        viewBox="0 0 28.125 36"
+                      >
+                        <path
+                          fill="#17bf88"
+                          d="M18 0L3.937 21.094h7.576V36h12.973V21.094h7.576z"
+                          transform="translate(-3.937)"
+                        ></path>
+                      </svg>
+                    </span>{' '}
+                    <p
+                      style={{
+                        fontSize: '48px',
+                        fontWeight: 'bold',
+                        color: TypeOfPnLColorHex.PROFIT,
+                        marginTop: '30px',
+                        marginBottom: '10px',
+                        fontFamily: 'barlow',
+                      }}
+                    >
+                      {randomPnL} %
+                    </p>
+                  </div>
+                  <div
                     style={{
-                      fontSize: '3.125rem',
-                      fontWeight: '900',
-                      color: TypeOfPnLColorHex.PROFIT,
-                      marginTop: '1.25rem',
-                      marginBottom: '0.75rem',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      gap: '0px',
+                      fontFamily: 'barlow',
                     }}
                   >
-                    + {randomPnL} %
-                  </p>
-                  <div style={{display: 'flex', flexDirection: 'column', gap: '0.5rem'}}>
                     {/* Rest of the code */}
+                    <div style={{display: 'flex', gap: '0.5rem', justifyContent: 'flex-end'}}>
+                      <p
+                        style={{
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#8B8E91',
+                          fontFamily: 'barlow',
+                        }}
+                      >
+                        Open Price
+                      </p>
+                      <p
+                        style={{
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#fff',
+                          fontFamily: 'barlow',
+                        }}
+                      >
+                        {openPrice}
+                        <span
+                          style={{
+                            marginTop: '2px',
+                            fontSize: '12px',
+                            fontWeight: 'lighter',
+                            color: '#8B8E91',
+                            fontFamily: 'barlow',
+                          }}
+                        >
+                          USDT
+                        </span>
+                      </p>
+                    </div>
+
+                    <div style={{display: 'flex', gap: '0.5rem'}}>
+                      <p
+                        style={{
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#8B8E91',
+                          fontFamily: 'barlow',
+                        }}
+                      >
+                        Close Price
+                      </p>
+                      <p
+                        style={{
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#fff',
+                          fontFamily: 'barlow',
+                        }}
+                      >
+                        {closePrice}
+                      </p>
+                    </div>
+
+                    <div style={{display: 'flex', gap: '0.5rem'}}>
+                      <p
+                        style={{
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#8B8E91',
+                          fontFamily: 'barlow',
+                        }}
+                      >
+                        Leverage
+                      </p>
+                      <p
+                        style={{
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#fff',
+                          fontFamily: 'barlow',
+                        }}
+                      >
+                        {leverage}x
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>

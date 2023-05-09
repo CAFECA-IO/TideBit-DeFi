afaf27babfbc8f34c65f0ca8c127deee7a17cf9d
arealclimber <luffulgence@gmail.com>
2023-05-09 16:03:23 +0800

    feat: 🎸 change DOMAIN to WWW one

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -261,7 +261,7 @@ export const TRADING_CRYPTO_DATA = [
   },
 ];
 export const SERVICE_TERM_TITLE = 'ServiceTerm';
-export const DOMAIN = 'https://tidebit-defi.com';
+export const DOMAIN = 'https://www.tidebit-defi.com';
 export const TERM_OF_SERVICE = DOMAIN + '{hash}';
 export const PRIVATE_POLICY = DOMAIN + '{hash}';
 export const DeWT_VALIDITY_PERIOD = 60 * 60; // seconds

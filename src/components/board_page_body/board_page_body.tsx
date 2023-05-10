94ac01878352e5229c11c1e43380fec50cb1140a
Julian <julian890701@gmail.com>
2023-05-10 18:28:19 +0800

    develop leaderboard tab

--- src/components/board_page_body/board_page_body.tsx
+++ src/components/board_page_body/board_page_body.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserPersonalRanking from '../user_personal_ranking/user_personal_ranking';
+import LeaderboardTab from '../leaderboard_tab/leaderboard_tab';
 import Footer from '../footer/footer';
 import {useTranslation} from 'next-i18next';
 
@@ -12,13 +13,10 @@ const BoardPageBody = () => {
     <div className="overflow-x-hidden overflow-y-hidden pt-20">
       <div className="min-h-screen">
         <div className="absolute mx-auto my-10 flex w-screen flex-col items-center space-y-4">
-          <div>
-            <h1 className="text-3xl">{t('LEADERBOARD_PAGE.TITLE')}</h1>
+          <h1 className="text-3xl">{t('LEADERBOARD_PAGE.TITLE')}</h1>
+          <div className="pt-250px md:pt-300px">
+            <LeaderboardTab />
           </div>
-          {/* ToDo: (20230509 - Julian) Top 3 */}
-          <div className="h-500px w-700px bg-lightWhite"></div>
-          {/* ToDo: (20230509 - Julian) Ranking List with tabs */}
-          <div className="h-50px w-screen rounded-xl bg-darkGray7 md:w-8/10"></div>
         </div>
         <div className="relative flex justify-center">
           <UserPersonalRanking />

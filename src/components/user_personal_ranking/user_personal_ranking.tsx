b7f826575924384bf149689737722f6ab58b4620
Julian <julian890701@gmail.com>
2023-05-10 15:02:19 +0800

    personal ranking is done

--- src/components/user_personal_ranking/user_personal_ranking.tsx
+++ src/components/user_personal_ranking/user_personal_ranking.tsx
@@ -25,7 +25,7 @@ const UserPersonalRanking = () => {
       <div className={TypeOfPnLColor.EQUAL}>{numberFormatted(pnl)} %</div>
     );
 
-  const displayedRankingNumber =
+  const displayedCumulativePnl =
     cumulativePnl > 0 ? (
       <div className="text-lightYellow2">+ {numberFormatted(cumulativePnl)} %</div>
     ) : cumulativePnl < 0 ? (
@@ -34,7 +34,6 @@ const UserPersonalRanking = () => {
       <div className="text-lightYellow2">{numberFormatted(cumulativePnl)} %</div>
     );
 
-  /* ToDo: (20230509 - Julian) default icon */
   const displayedArrow =
     cumulativePnl > 0 ? (
       <ImArrowUp width={20} height={26} />
@@ -46,25 +45,27 @@ const UserPersonalRanking = () => {
 
   const personalRanking = userCtx.wallet ? (
     <div className="fixed bottom-0 z-30 flex h-80px w-screen whitespace-nowrap bg-darkGray3 px-4 md:w-8/10">
-      <div className="flex flex-1 items-center space-x-4">
-        <div className="inline-flex items-center">
+      <div className="flex flex-1 items-center space-x-2 md:space-x-4">
+        <div className="inline-flex items-center sm:w-70px">
           <Image src="/elements/crown.svg" width={25} height={25} alt="crown_icon" />
 
-          <div className="ml-2 text-lg">{rankingNumber}</div>
+          <div className="ml-2 text-sm sm:text-lg">{rankingNumber}</div>
         </div>
-        <div className="relative inline-flex h-60px w-60px items-center justify-center overflow-hidden rounded-full bg-tidebitTheme text-center">
-          <span className="text-3xl font-bold text-lightWhite">{username}</span>
+        {/* Info: (20230510 - Julian) User Avatar */}
+        <div className="relative inline-flex h-36px w-36px items-center justify-center overflow-hidden rounded-full bg-tidebitTheme text-center md:h-60px md:w-60px">
+          <span className="text-2xl font-bold text-lightWhite md:text-3xl">{username}</span>
         </div>
-        <div className="truncate text-xl">{accountTruncate(userCtx.wallet)}</div>
+        {/* Info: (20230510 - Julian) User Name */}
+        <div className="truncate text-sm sm:text-xl">{accountTruncate(userCtx.wallet)}</div>
       </div>
-      <div className="flex items-center space-x-3 text-xl">
+      <div className="flex items-center space-x-3 text-base md:text-xl">
         <div className="inline-flex items-end">
           {displayedPnl}
           <span className="ml-1 text-sm text-lightGray4">{unitAsset}</span>
         </div>
-        <div className="inline-flex items-center space-x-3 text-lightYellow2">
-          <div>{displayedRankingNumber}</div>
-          {displayedArrow}
+        <div className="inline-flex items-center space-x-1 text-sm text-lightYellow2 sm:text-lg md:space-x-3">
+          <div className="hidden sm:block">{displayedCumulativePnl}</div>
+          <div>{displayedArrow}</div>
           <div>{rankingNumber}</div>
         </div>
       </div>

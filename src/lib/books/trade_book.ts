a8d64986432f2ea1402d51012b87e41d51f5d548
arealclimber <luffulgence@gmail.com>
2023-05-18 18:22:11 +0800

    test: 💍 #626

--- src/lib/books/trade_book.ts
+++ src/lib/books/trade_book.ts
@@ -1,31 +1,25 @@
-interface Order {
-  id: string;
-  from: number;
-  to: number;
-  timestamp: number;
-}
-
 /**
  * @dev timestamp in ms
  */
-interface Trade {
+interface ITrade {
   tradeId: string;
   targetAsset: string;
   unitAsset: string;
   direct: string;
   price: number;
   // quantity: number;
-  timestamp: number;
+  timestampMs: number;
   quantity: number;
+  // predictionCounter?: number;
 }
 
-type line = {
+type ILine = {
   average: number;
   volume: number;
   timestamp: number;
 };
 
-type candleStick = {
+type ICandlestick = {
   open: number;
   high: number;
   low: number;
@@ -34,81 +28,174 @@ type candleStick = {
   timestamp: number;
 };
 
-class OrderBook {
-  private orders: Order[];
-
-  constructor() {
-    this.orders = [];
-  }
-
-  add(order: Order): void {
-    this.orders.push(order);
-  }
-
-  update(id: string, order: Order): void {
-    let index = -1;
-    for (let i = 0; i < this.orders.length; i++) {
-      if (this.orders[i].id === id) {
-        index = i;
-        break;
-      }
-    }
-
-    if (index !== -1) {
-      this.orders[index] = order;
-    }
-  }
-
-  remove(id: string): void {
-    this.orders = this.orders.filter(o => o.id !== id);
-  }
-}
+type IAveragedTrade = {
+  timestampMs: number;
+  averagePrice: number;
+};
 
 class TradeBook {
-  private trades: Trade[];
+  private trades: ITrade[];
+  private predictionCounter: number;
+  private isPredicting: boolean;
+  private predictionTimer: any;
 
+  private model: (x: number) => ITrade | undefined;
+
+  // INFO: 決定要用哪個預測的模型，模型名字是 constant [later]
+
+  // Info: new object 時執行的 code
   constructor() {
     this.trades = [];
+    this.isPredicting = false;
+    this.predictionCounter = 0;
+    this.model = this.linearRegression.bind(this);
   }
 
-  add(trade: Trade): void {
+  // Info: 真的 trade
+  add(trade: ITrade): void {
+    this.resetPrediction();
+
+    // Info: `_` 為了避免無窮迴圈，所以區分對外的接口跟實際的功能
+    this._add(trade);
+    this.startPredictionLoop();
+  }
+
+  private resetPrediction() {
+    clearTimeout(this.predictionTimer);
+    this.predictionCounter = 0;
+  }
+
+  private _add(trade: ITrade): void {
     this.trades.push(trade);
   }
 
+  private startPredictionLoop = () => {
+    const periodMs = 100;
+
+    // INFO: setTimeout + 遞迴
+    // 有新資料時，重置 predictionCounter
+    const predictionLoop = () => {
+      if (this.isPredicting) {
+        this.predictNextTrade(periodMs, this.model);
+        this.predictionTimer = setTimeout(predictionLoop, periodMs);
+      }
+    };
+    predictionLoop();
+
+    // INFO: setInterval [trial]
+    // const intervalId = setInterval(() => {
+    //   if (this.isPredicting) {
+    //     this.predictNextTrade(periodMs, this.model);
+    //   } else {
+    //     clearInterval(intervalId);
+    //   }
+    // }, periodMs);
+  };
+
   // TODO: Create a function to accept prediction modal as parameter and return the predicted price
-  public predictNextTradePrice(period: number, predictionModel: (x: number) => number): number {
-    const predictedPrice = predictionModel(period);
-    return predictedPrice;
+  predictNextTrade(periodMs: number, model: (x: number) => ITrade | undefined) {
+    const trade = model(periodMs);
+    if (trade !== undefined) {
+      this._add(trade);
+    }
   }
 
-  public linearRegression(period: number): number {
-    if (this.trades.length < 2) throw new Error('Not enough trades to make a prediction');
+  linearRegression(periodMs: number): ITrade | undefined {
+    const groupedTrades: {[key: number]: ITrade[]} = {};
 
-    // Info: linear regression
-    const t2 = this.trades[this.trades.length - 2]; // 倒數第二
-    const t1 = this.trades[this.trades.length - 1]; // 倒數第一
+    // console.log('trades', this.trades);
 
-    if (t2.timestamp === t1.timestamp) {
-      const t3 = this.trades[this.trades.length - 3]; // 倒數第三
-      // TODO: Decimal places
-      const avgPrice = (t2.price + t1.price) / 2;
-      const priceDifference = avgPrice - t3.price;
-      const timeDifference = t1.timestamp - t3.timestamp;
-      return t1.price + (priceDifference / timeDifference) * period;
+    this.trades.forEach(trade => {
+      if (!groupedTrades[trade.timestampMs]) {
+        groupedTrades[trade.timestampMs] = [];
+      }
+      groupedTrades[trade.timestampMs].push(trade);
+    });
+
+    if (Object.keys(groupedTrades).length < 2) return;
+
+    const averagedTrades: IAveragedTrade[] = Object.entries(groupedTrades).map(
+      ([timestamp, trades]) => {
+        const sumPrice = trades.reduce((sum, trade) => sum + trade.price, 0);
+        const averagePrice = sumPrice / trades.length; // TODO: check if this is correct
+        return {
+          timestampMs: Number(timestamp),
+          averagePrice,
+        };
+      }
+    );
+
+    // Info: Ensure there are at least two trades with different timestamps
+    if (averagedTrades.length < 2) {
+      return;
     }
 
-    const priceDifference = t1.price - t2.price;
-    const timeDifference = t1.timestamp - t2.timestamp;
-    return t1.price + (priceDifference / timeDifference) * period;
+    const t2 = averagedTrades[averagedTrades.length - 2]; // Info: 倒數第二個
+    const t1 = averagedTrades[averagedTrades.length - 1]; // Info: 倒數第一個
+
+    const priceDifference = t1.averagePrice - t2.averagePrice;
+    const timeDifference = t1.timestampMs - t2.timestampMs;
+
+    const lastTrade = this.trades[this.trades.length - 1];
+    // const secondLastTrade = this.trades[this.trades.length - 2];
+
+    if (this.predictionCounter === 0) {
+      this.predictionCounter = 1;
+    } else {
+      this.predictionCounter++;
+    }
+
+    const newTradeId = `${
+      lastTrade.tradeId.includes('-')
+        ? `${lastTrade.tradeId.split('-')[0]}-${this.predictionCounter}`
+        : `${lastTrade.tradeId}-${this.predictionCounter}`
+    }`;
+
+    // TODO: limit price to two decimal places [later]
+    const newTrade = {
+      tradeId: newTradeId,
+      targetAsset: lastTrade.targetAsset,
+      unitAsset: lastTrade.unitAsset,
+      direct: lastTrade.direct,
+      price: t1.averagePrice + (priceDifference / timeDifference) * periodMs,
+      timestampMs: lastTrade.timestampMs + periodMs,
+      quantity: 0,
+    };
+
+    return newTrade;
   }
 
-  toLineChart(interval: number, length: number): line[] {
-    const lines: line[] = [];
+  togglePredicting = (value?: boolean) => {
+    if (value === undefined) {
+      this.isPredicting = !this.isPredicting;
+    } else {
+      this.isPredicting = value;
+    }
+  };
+
+  get predicting() {
+    return this.isPredicting;
+  }
+
+  get predictionModel() {
+    return this.model;
+  }
+
+  get tradeData() {
+    return this.trades;
+  }
+
+  setPredictionModel(model: (x: number) => ITrade | undefined) {
+    this.model = model;
+  }
+
+  toLineChart(interval: number, length: number): ILine[] {
+    const lines: ILine[] = [];
     const intervalMs = interval * 1000;
-    const lastTimestamp = this.trades[this.trades.length - 1].timestamp;
+    const lastTimestamp = this.trades[this.trades.length - 1].timestampMs;
     const firstTimestamp = lastTimestamp - length * intervalMs;
     for (let i = firstTimestamp; i <= lastTimestamp; i += intervalMs) {
-      const trades = this.trades.filter(t => t.timestamp >= i && t.timestamp < i + intervalMs);
+      const trades = this.trades.filter(t => t.timestampMs >= i && t.timestampMs < i + intervalMs);
       if (trades.length > 0) {
         const average = trades.reduce((sum, t) => sum + t.price, 0) / trades.length;
         const volume = trades.reduce((sum, t) => sum + t.quantity, 0);
@@ -118,23 +205,23 @@ class TradeBook {
     return lines;
   }
 
-  toCandleStick(interval: number, length: number): candleStick[] {
-    const candleSticks: candleStick[] = [];
+  toCandlestick(interval: number, length: number): ICandlestick[] {
+    const candleSticks: ICandlestick[] = [];
     const intervalMs = interval * 1000;
-    const lastTimestamp = this.trades[this.trades.length - 1].timestamp;
+    const lastTimestamp = this.trades[this.trades.length - 1].timestampMs;
     const firstTimestamp = lastTimestamp - length * intervalMs;
 
-    let candleStick: candleStick | undefined;
+    let candlestick: ICandlestick | undefined;
     for (let i = firstTimestamp; i <= lastTimestamp; i += intervalMs) {
-      const trades = this.trades.filter(t => t.timestamp >= i && t.timestamp < i + intervalMs);
+      const trades = this.trades.filter(t => t.timestampMs >= i && t.timestampMs < i + intervalMs);
       if (trades.length > 0) {
         const open = trades[0].price;
         const close = trades[trades.length - 1].price;
         const high = Math.max(...trades.map(t => t.price));
         const low = Math.min(...trades.map(t => t.price));
         const volume = trades.reduce((sum, t) => sum + t.quantity, 0);
-        candleStick = {open, high, low, close, volume, timestamp: i};
-        candleSticks.push(candleStick);
+        candlestick = {open, high, low, close, volume, timestamp: i};
+        candleSticks.push(candlestick);
       }
     }
 
@@ -142,10 +229,11 @@ class TradeBook {
   }
 }
 
-const tradeBook = new TradeBook();
+const TradeBookInstance = new TradeBook();
 
 // Info: Price priority ------------------------------
-const tradeObj1: Trade = {
+
+const tradeObj1: ITrade = {
   tradeId: '1',
   targetAsset: 'ETH',
   unitAsset: 'USDT',
@@ -153,10 +241,10 @@ const tradeObj1: Trade = {
   // quantity: 17,
   price: 4.412,
   quantity: 17,
-  timestamp: 1683775038000,
+  timestampMs: 1683775038000,
 };
 
-const tradeObj2: Trade = {
+const tradeObj2: ITrade = {
   tradeId: '2',
   targetAsset: 'ETH',
   unitAsset: 'USDT',
@@ -164,142 +252,80 @@ const tradeObj2: Trade = {
   // quantity: 575,
   price: 8.929,
   quantity: 575,
-  timestamp: 1683775045000,
+  timestampMs: 1683775039000,
 };
 
-const tradeObj3: Trade = {
+const tradeObj3: ITrade = {
   tradeId: '3',
   targetAsset: 'ETH',
   unitAsset: 'USDT',
   direct: 'BUY',
   price: 9.143,
   quantity: 7,
-  timestamp: 1683775045000,
+  timestampMs: 1683775045000,
 };
 
-const tradeObj4: Trade = {
+const tradeObj4: ITrade = {
   tradeId: '4',
   targetAsset: 'ETH',
   unitAsset: 'USDT',
   direct: 'BUY',
   price: 11.833,
   quantity: 18,
-  timestamp: 1683775045000,
+  timestampMs: 1683775045000,
 };
 
-const tradeObj5: Trade = {
+const tradeObj5: ITrade = {
   tradeId: '5',
   targetAsset: 'ETH',
   unitAsset: 'USDT',
   direct: 'BUY',
   price: 11.833,
   quantity: 33,
-  timestamp: 1683775054000,
+  timestampMs: 1683775054000,
 };
 
-const tradeObj6: Trade = {
+const tradeObj6: ITrade = {
   tradeId: '6',
   targetAsset: 'ETH',
   unitAsset: 'USDT',
   direct: 'BUY',
   price: 11.833,
   quantity: 9,
-  timestamp: 1683775072000,
+  timestampMs: 1683775072000,
 };
 
-const tradeObj7: Trade = {
+const tradeObj7: ITrade = {
   tradeId: '7',
   targetAsset: 'ETH',
   unitAsset: 'USDT',
   direct: 'BUY',
   price: 17.927,
   quantity: 12,
-  timestamp: 1683775072000,
+  timestampMs: 1683775072000,
 };
+TradeBookInstance.togglePredicting();
 
-/* Info: Time priority ------------------------------
-// const tradeObj1: Trade = {
-// 	tradeId: '1',
-// 	targetAsset: 'ETH',
-// 	unitAsset: 'USDT',
-// 	direct: 'buy',
-// 	// quantity: 17,
-// 	price: 4.412,
-// 	quantity: 17,
-// 	timestamp: 1683775038000,
-// };
+TradeBookInstance.add(tradeObj1);
+TradeBookInstance.add(tradeObj2);
 
-// const tradeObj2: Trade = {
-// 	tradeId: '2',
-// 	targetAsset: 'ETH',
-// 	unitAsset: 'USDT',
-// 	direct: 'sell',
-// 	// quantity: 575,
-// 	price: 8.929,
-// 	quantity: 575,
-// 	timestamp: 1683775045000,
-// };
+// TradeBookInstance.togglePredicting();
 
-// const tradeObj3: Trade = {
-// 	tradeId: '3',
-// 	targetAsset: 'ETH',
-// 	unitAsset: 'USDT',
-// 	direct: 'BUY',
-// 	price: 11.833,
-// 	quantity: 25,
-// 	timestamp: 1683775045000,
-// };
+// TradeBookInstance.add(tradeObj5);
 
-// const tradeObj4: Trade = {
-// 	tradeId: '4',
-// 	targetAsset: 'ETH',
-// 	unitAsset: 'USDT',
-// 	direct: 'BUY',
-// 	price: 11.849,
-// 	quantity: 33,
-// 	timestamp: 1683775045000,
-// };
+// TradeBookInstance.add(tradeObj6);
+// TradeBookInstance.add(tradeObj7);
 
-// const tradeObj5: Trade = {
-// 	tradeId: '5',
-// 	targetAsset: 'ETH',
-// 	unitAsset: 'USDT',
-// 	direct: 'BUY',
-// 	price: 11.849,
-// 	quantity: 2,
-// 	timestamp: 1683775054000,
-// };
+// setTimeout(() => {
+// 	TradeBookInstance.add(tradeObj3);
+// 	TradeBookInstance.add(tradeObj4);
+// 	// TradeBookInstance.tradeData;
+// }, 1000);
 
-// const tradeObj6: Trade = {
-// 	tradeId: '6',
-// 	targetAsset: 'ETH',
-// 	unitAsset: 'USDT',
-// 	direct: 'BUY',
-// 	price: 9.143,
-// 	quantity: 7,
-// 	timestamp: 1683775072000,
-// };
+setTimeout(() => {
+  TradeBookInstance.togglePredicting(false);
+}, 10000);
 
-// const tradeObj7: Trade = {
-// 	tradeId: '7',
-// 	targetAsset: 'ETH',
-// 	unitAsset: 'USDT',
-// 	direct: 'BUY',
-// 	price: 17.927,
-// 	quantity: 12,
-// 	timestamp: 1683775072000,
-// };
-------------------------------*/
+// Info: Time priority ------------------------------
 
-tradeBook.add(tradeObj1);
-
-tradeBook.add(tradeObj2);
-
-tradeBook.add(tradeObj3);
-
-tradeBook.add(tradeObj4);
-tradeBook.add(tradeObj5);
-tradeBook.add(tradeObj6);
-tradeBook.add(tradeObj7);
-
-export default tradeBook;
+export default TradeBookInstance;

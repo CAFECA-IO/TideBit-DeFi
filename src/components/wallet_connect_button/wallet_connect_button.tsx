0577981a86749a7a5adb360fbe6be34532fd8cf1
Julian <julian890701@gmail.com>
2023-06-13 11:14:16 +0800

    fix #873

--- src/components/wallet_connect_button/wallet_connect_button.tsx
+++ src/components/wallet_connect_button/wallet_connect_button.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React from 'react';
 import TideButton from '../tide_button/tide_button';
 import {useTranslation} from 'react-i18next';
 import {TranslateFunction} from '../../interfaces/tidebit_defi_background/locale';
@@ -6,7 +6,7 @@ import useScanProcess from '../../lib/hooks/use_scan_process';
 
 export const WalletConnectButton = ({className}: {className?: string}) => {
   const {t}: {t: TranslateFunction} = useTranslation('common');
-  const {scanned, setScanned, scanProcess} = useScanProcess();
+  const {scanProcess} = useScanProcess();
 
   return (
     <TideButton

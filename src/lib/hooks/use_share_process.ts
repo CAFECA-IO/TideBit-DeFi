550a3fc8dd9e16b32eae988c3ac404bb631f9078
Julian <julian890701@gmail.com>
2023-07-14 15:43:33 +0800

    share badge done

--- src/lib/hooks/use_share_process.ts
+++ src/lib/hooks/use_share_process.ts
@@ -56,7 +56,7 @@ const useShareProcess = ({lockerName, shareType, shareId, cfd, enableShare}: IUs
         return shareUrl;
 
       case ShareType.BADGE:
-        shareUrl = `https://tidebit-defi-o7sfp9uq6-cafeca.vercel.app/share/badge/${shareId}`; //DOMAIN + `/share/badge/${shareId}`;
+        shareUrl = `https://tidebit-defi-1gnq146up-cafeca.vercel.app/share/badge/${shareId}`; //DOMAIN + `/share/badge/${shareId}`;
         return shareUrl;
 
       case ShareType.ARTICLE:

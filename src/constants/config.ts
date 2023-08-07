007c3667f9b8e3842521b0e49adae1b8138be251
arealclimber <luffulgence@gmail.com>
2023-08-07 15:30:40 +0800

    test: 💍 test domain

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -275,7 +275,7 @@ export const TRADING_CRYPTO_DATA = [
 ];
 export const SERVICE_TERM_TITLE = 'ServiceTerm';
 export const DOMAIN = 'https://tidebit-defi.com';
-export const SHARE_DOMAIN = 'https://tidebit-defi-4clzcpasp-cafeca.vercel.app';
+export const SHARE_DOMAIN = 'https://tidebit-defi-26b399q1g-cafeca.vercel.app';
 export const TERM_OF_SERVICE = DOMAIN + '{hash}';
 export const PRIVATE_POLICY = DOMAIN + '{hash}';
 export const DeWT_VALIDITY_PERIOD = 60 * 60; // seconds

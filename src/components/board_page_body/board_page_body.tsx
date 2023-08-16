a7455fac7840373900474a9d8be9f60661e66ef2
Julian <julian890701@gmail.com>
2023-08-16 17:23:43 +0800

    leaderboard time zone

--- src/components/board_page_body/board_page_body.tsx
+++ src/components/board_page_body/board_page_body.tsx
@@ -61,6 +61,11 @@ const BoardPageBody = () => {
     return () => clearTimeout(timer);
   }, [leaderboardLiveRemains]);
 
+  const timeSpanStart = new Date(timestampToString(startTime).date).toISOString().slice(0, 10);
+  const timeSpanEnd = new Date(timestampToString(endTime).date).toISOString().slice(0, 10);
+  /* Info: (20230816 - Julian) Trick: 月榜的時間轉換由起始時間的第二天算，以避免時區誤差 */
+  const timeSpanMonthly = timestampToString(startTime + 86400).monthAndYear;
+
   const subtitle =
     timeSpan === RankingInterval.LIVE ? (
       <div className="inline-block text-base md:text-xl">
@@ -71,21 +76,21 @@ const BoardPageBody = () => {
     ) : timeSpan === RankingInterval.DAILY ? (
       <div className="inline-block text-base md:text-xl">
         {t('LEADERBOARD_PAGE.SUBTITLE_DAILY')}{' '}
-        <span className="text-tidebitTheme">{timestampToString(endTime).date}</span>
+        <span className="text-tidebitTheme">{timeSpanEnd}</span>
         {t('LEADERBOARD_PAGE.SUBTITLE_DAILY_2')}
       </div>
     ) : timeSpan === RankingInterval.WEEKLY ? (
       <div className="inline-block text-base md:text-xl">
         {t('LEADERBOARD_PAGE.SUBTITLE_WEEKLY')}{' '}
-        <span className="text-tidebitTheme">{timestampToString(startTime).date}</span>{' '}
+        <span className="text-tidebitTheme">{timeSpanStart}</span>{' '}
         {t('LEADERBOARD_PAGE.SUBTITLE_WEEKLY_2')}{' '}
-        <span className="text-tidebitTheme">{timestampToString(endTime).date}</span>{' '}
+        <span className="text-tidebitTheme">{timeSpanEnd}</span>{' '}
         {t('LEADERBOARD_PAGE.SUBTITLE_WEEKLY_3')}
       </div>
     ) : timeSpan === RankingInterval.MONTHLY ? (
       <div className="inline-block text-base md:text-xl">
         {t('LEADERBOARD_PAGE.SUBTITLE_MONTHLY')}{' '}
-        <span className="text-tidebitTheme">{timestampToString(endTime).monthAndYear}</span>
+        <span className="text-tidebitTheme">{timeSpanMonthly}</span>
         {t('LEADERBOARD_PAGE.SUBTITLE_MONTHLY_2')}
       </div>
     ) : (

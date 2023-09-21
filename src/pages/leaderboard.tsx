f33617009ae2fe720d74b66a17cd29577022b8cd
Julian <julian890701@gmail.com>
2023-09-21 10:44:22 +0800

    remove unused

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -4,13 +4,10 @@ import Head from 'next/head';
 import BoardPageBody from '../components/board_page_body/board_page_body';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
 import {AppContext} from '../contexts/app_context';
-import {useGlobal} from '../contexts/global_context';
 import {ILocale} from '../interfaces/tidebit_defi_background/locale';
 import {TIDEBIT_FAVICON} from '../constants/display';
-import {LayoutAssertion} from '../constants/layout_assertion';
 
 const Leaderboard = () => {
-  const {layoutAssertion} = useGlobal();
   const displayedNavBar = <NavBar />;
 
   const appCtx = useContext(AppContext);

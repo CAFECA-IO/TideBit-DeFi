cfc7922434c40b2755e47eca8fdfd9f2e273728d
arealclimber <luffulgence@gmail.com>
2023-10-03 13:23:44 +0800

    test: 💍 postCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 // postcss.config.js
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    // ...other PostCSS plugins,
-    process.env.NODE_ENV === 'production' ? require('cssnano')({preset: 'default'}) : false,
-  ].filter(Boolean), // This will remove any falsey values from the plugins array, which can be useful if you are conditionally adding plugins.
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? {cssnano: {preset: 'default'}} : {}),
+  },
 };

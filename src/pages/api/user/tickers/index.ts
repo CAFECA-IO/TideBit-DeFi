47b235c2794b954b64663e76a15d3a1365e9647e
arealclimber <luffulgence@gmail.com>
2023-10-13 15:29:58 +0800

    test: 💍 set&get cookie in the api of add/rm/get favorites
    ✅ Closes: #1329

--- src/pages/api/user/tickers/index.ts
+++ src/pages/api/user/tickers/index.ts
@@ -1,20 +1,39 @@
 import {NextApiRequest, NextApiResponse} from 'next';
 import {Ticker} from '../../../../constants/ticker';
+import cookie from 'cookie';
 
-let favoriteTickers = Object.values(Ticker);
+const defaultFavoriteTickers = Object.values(Ticker) as string[];
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const cookies = cookie.parse(req.headers.cookie || '');
+
+  let favoriteTickers = cookies.favoriteTickers
+    ? JSON.parse(cookies.favoriteTickers)
+    : defaultFavoriteTickers;
+
   if (req.method === 'GET') {
     res.status(200).json(favoriteTickers);
   } else if (req.method === 'PUT') {
     const success = Math.random() > 0.3 ? true : false;
     if (req.body.starred === true) {
       favoriteTickers = favoriteTickers.concat(req.body.instId);
-      res.status(200).json({favoriteTickers, success});
     } else if (req.body.starred === false) {
-      favoriteTickers = favoriteTickers.filter(t => !req.body.instId?.includes(t));
-      res.status(200).json({favoriteTickers, success});
+      favoriteTickers = favoriteTickers.filter((t: string) => !req.body.instId?.includes(t));
     } else res.status(500).json({error: 'Internal Server Error'});
-    // Deprecated: Causing error `Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client` (20230727 - Shirley)
-    // res.status(200).json({success: Math.random() > 0.3 ? true : false});
+
+    const expiration = new Date();
+    expiration.setDate(expiration.getDate() + 365);
+
+    res.setHeader(
+      'Set-Cookie',
+      cookie.serialize('favoriteTickers', JSON.stringify(favoriteTickers), {
+        httpOnly: true,
+        path: '/',
+        expires: expiration,
+        sameSite: 'strict',
+      })
+    );
+
+    res.status(200).json({favoriteTickers, success});
   }
 }

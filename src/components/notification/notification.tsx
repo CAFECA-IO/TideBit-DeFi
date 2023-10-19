6b815fb43f2109df5a2695c5c8b82d5da64139f1
Julian <julian890701@gmail.com>
2023-10-19 12:23:22 +0800

    fix notification

--- src/components/notification/notification.tsx
+++ src/components/notification/notification.tsx
@@ -50,9 +50,9 @@ export default function Notification({
     return () => clearTimeout(timer);
   };
 
-  const displayedNotificationList = notificationCtx.unreadNotifications.map(v => {
+  const displayedNotificationList = notificationCtx.unreadNotifications.map((v, index) => {
     return (
-      <div key={v.id + v} className={`${readAllanim} transition-all duration-300 ease-in-out`}>
+      <div key={index} className={`${readAllanim} transition-all duration-300 ease-in-out`}>
         <NotificationItem
           id={v.id}
           title={v.title}
@@ -67,78 +67,42 @@ export default function Notification({
     );
   });
 
-  /* Info: (20230420 - Julian) Desktop notification drawer */
-  const isDisplayedNotificationSidebarSection = (
-    <>
-      {/* Info: (20230420 - Julian) sidebar section */}
+  return (
+    <div
+      className={`pointer-events-none fixed right-0 top-0 z-60 flex min-h-screen overflow-x-hidden overflow-y-hidden`}
+    >
+      {/* Info: (20230420 - Julian) sidebar self */}
       <div
-        className={`pointer-events-none fixed right-1 top-44px ${
-          componentVisible ? 'z-60' : 'z-60'
-        } flex overflow-x-hidden overflow-y-hidden outline-none focus:outline-none`}
+        ref={notifyRef}
+        className={`pointer-events-auto mt-60px w-screen ${`lg:w-479px`} ${
+          componentVisible
+            ? 'visible opacity-100 lg:translate-x-0'
+            : 'invisible opacity-10 lg:translate-x-full'
+        } flex flex-col bg-darkGray p-4 text-white transition-all duration-100 sm:p-5 lg:bg-darkGray/90 lg:duration-300`}
       >
-        <div className="relative mx-auto my-3 w-auto">
-          {' '}
-          <div className={`relative`}>
-            {/* Info: (20230420 - Julian) sidebar self */}
-            <div
-              ref={notifyRef}
-              className={`pointer-events-auto min-h-screen w-screen ${`lg:w-479px`} ${
-                componentVisible
-                  ? 'visible opacity-100 lg:translate-x-0'
-                  : 'invisible opacity-10 lg:translate-x-full'
-              } flex flex-col bg-darkGray pb-20 pt-8 text-white transition-all duration-100 sm:p-5 lg:bg-darkGray/90 lg:duration-300`}
-            >
-              <div className="mb-10 flex items-center">
-                <h1 className="hidden justify-start text-2xl font-bold lg:flex lg:w-1/2 lg:shrink-0">
-                  {t('NAV_BAR.NOTIFICATION_TITLE')}
-                </h1>
-                <div
-                  className="flex w-full shrink-0 justify-end text-sm text-tidebitTheme underline hover:cursor-pointer lg:w-1/2"
-                  onClick={readAllHandler}
-                >
-                  {t('NAV_BAR.NOTIFICATION_READ_ALL')}
-                </div>
-              </div>
-
-              {/* Info: (20230420 - Julian) Notification List*/}
-              <div className="flex h-80vh w-full flex-col space-y-5 overflow-y-auto overflow-x-hidden pb-40 sm:pb-10">
-                {displayedNotificationList}
-              </div>
-            </div>
+        {/* Info: (20231019 - Julian) Notification Title & Read All */}
+        <div className="my-4 flex items-center">
+          <h1 className="hidden justify-start text-2xl font-bold lg:flex lg:w-1/2 lg:shrink-0">
+            {t('NAV_BAR.NOTIFICATION_TITLE')}
+          </h1>
+          <div
+            className="flex w-full shrink-0 justify-end text-sm text-tidebitTheme underline hover:cursor-pointer lg:w-1/2"
+            onClick={readAllHandler}
+          >
+            {t('NAV_BAR.NOTIFICATION_READ_ALL')}
           </div>
         </div>
-      </div>
-    </>
-  );
 
-  /* Info: (20230420 - Julian) Cover for Desktop notification drawer */
-  const isDisplayedNotificationSidebarCover = componentVisible ? (
-    <>
-      {/* Info: (20230420 - Julian) cover for NavBar ***Bell Icon*** */}
-      <div
-        className={`${
-          componentVisible ? 'visible duration-700 ease-in-out' : 'invisible'
-        } invisible fixed z-80 flex h-16 items-center justify-center overflow-x-hidden overflow-y-hidden bg-transparent outline-none hover:cursor-pointer focus:outline-none lg:visible lg:right-52 lg:top-6 lg:h-8 lg:w-8`}
-      >
-        {' '}
+        {/* Info: (20230420 - Julian) Notification List */}
+        <div className="flex h-80vh w-full flex-col space-y-5 overflow-y-auto overflow-x-hidden pb-40 pt-5 sm:pb-10">
+          {displayedNotificationList}
+          {displayedNotificationList}
+          {displayedNotificationList}
+          {displayedNotificationList}
+          {displayedNotificationList}
+          {displayedNotificationList}
+        </div>
       </div>
-
-      {/* Info: (20230420 - Julian) cover for Main, Footer */}
-      <div
-        className={`${
-          componentVisible ? 'transition-opacity duration-700 ease-in-out' : 'invisible'
-        } fixed inset-0 z-30 flex items-center justify-center overflow-x-hidden overflow-y-hidden bg-darkGray/10 outline-none backdrop-blur-sm focus:outline-none`}
-      >
-        {' '}
-      </div>
-    </>
-  ) : null;
-
-  return (
-    <div>
-      {/* Info: (20230420 - Julian) Notification Sidebar */}
-      {isDisplayedNotificationSidebarCover}
-      {isDisplayedNotificationSidebarSection}
     </div>
   );
 }

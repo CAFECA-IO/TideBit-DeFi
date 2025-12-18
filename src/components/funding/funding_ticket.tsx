563e912767b404ceb9153a0de5bb1c52382bde5e
julian <julian890701@gmail.com>
2025-12-18 18:29:43 +0800

    develop funding ticket

--- src/components/funding/funding_ticket.tsx
+++ src/components/funding/funding_ticket.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
+import { FiLock } from 'react-icons/fi';
 import { IFundingItemUI } from '@/interfaces/funding';
 import { numberWithCommas } from '@/lib/utils/common';
 import ProgressBar, { ProgressBarColor, ProgressBarSize } from '@/components/common/progress_bar';
@@ -24,6 +25,7 @@ const FundingTicket: React.FC<IFundingTicketProps> = ({ data }) => {
     committedFundAmount,
     committedTokensCount,
     investorsCount,
+    isLocked,
   } = data;
 
   const progressPercentage = (raisedFundingAmount / goalFundingAmount) * 100;
@@ -35,65 +37,105 @@ const FundingTicket: React.FC<IFundingTicketProps> = ({ data }) => {
     value: committedFundAmount,
     label: 'Tokens / 10K',
   };
-
   const centerStatData: StatDisplay = {
     value: committedTokensCount,
     label: 'Investors',
   };
-
   const rightStatData: StatDisplay = {
     value: investorsCount,
     label: 'Days',
   };
 
-  return (
-    <div className="flex flex-col overflow-hidden rounded-radius-l bg-surface-neutral-container-lv2">
-      {/* Info: (202501218 - Julian) Cover Image */}
-      <div className="relative h-180px w-full shrink-0">
-        <Image src={coverImageId} alt="Funding Cover" fill objectFit="cover" />
+  // Info: (202501218 - Julian) 根據是否鎖定，調整文字樣式
+  const statValueStyle = `${isLocked ? 'text-text-state-mute' : 'text-text-neutral-primary'} text-lg font-bold`;
+  const statLabelStyle = `${isLocked ? 'text-text-state-mute' : 'text-text-neutral-tertiary'} text-xs font-normal`;
 
-        {/* Info: (202501218 - Julian) Token & Industry */}
-        <div className="absolute bottom-0 left-0 w-full px-spacing-lv-4 py-spacing-lv-3">
-          <div className="flex items-center justify-between">
-            <div className="flex items-end gap-spacing-lv-0 text-text-neutral-primary">
-              <p className="text-2xl font-bold">$ {tokenPrice}</p>
-              <p className="text-xs font-normal">/{tokenName}</p>
-            </div>
-            <div className="rounded-radius-rounded bg-badge-brand-secondary px-spacing-lv-2 py-spacing-lv-0 text-xs font-bold text-badge-brand-on-secondary">
-              {industry}
-            </div>
+  const displayedBudge = (
+    <div
+      className={`${
+        isLocked
+          ? 'bg-badge-neutral-light text-badge-neutral-on-light'
+          : 'bg-badge-brand-secondary text-badge-brand-on-secondary'
+      } rounded-radius-rounded px-spacing-lv-2 py-spacing-lv-0 text-xs font-bold`}
+    >
+      {industry}
+    </div>
+  );
+
+  const displayedCoverOverlay = (
+    <div
+      className={` ${
+        isLocked ? 'bg-opacity-neutral-dark-80' : 'bg-transparent'
+      } absolute bottom-0 left-0 flex size-full flex-col justify-end px-spacing-lv-4 py-spacing-lv-3`}
+    >
+      {isLocked && (
+        <div className="ml-auto text-icon-neutral-primary">
+          <FiLock size={24} />
+        </div>
+      )}
+      <div className="mt-auto">
+        <div className="flex items-center justify-between">
+          <div
+            className={`${isLocked ? 'text-text-state-mute' : 'text-text-neutral-primary'} flex items-end gap-spacing-lv-0`}
+          >
+            <p className="text-2xl font-bold">$ {tokenPrice}</p>
+            <p className="text-xs font-normal">/{tokenName}</p>
           </div>
+          {displayedBudge}
         </div>
       </div>
+    </div>
+  );
 
-      {/* Info: (202501218 - Julian) Content */}
-      <div className="flex flex-col">
-        {/* Info: (202501218 - Julian) Company Name and Title */}
-        <div className="flex flex-col gap-spacing-lv-0 px-spacing-lv-6 py-spacing-lv-3">
-          <p className="text-xs font-normal text-text-neutral-tertiary">{companyName}</p>
-          <p className="font-bold text-text-neutral-primary">{title}</p>
+  return (
+    <div className="relative">
+      {/* ToDo: (202501218 - Julian) 調整位置 */}
+      <div className="absolute z-10">
+        <Image src="/icons/committed_mark.svg" width={36} height={40} alt="committed mark" />
+      </div>
+      <div className="flex flex-col overflow-hidden rounded-radius-l bg-surface-neutral-container-lv2">
+        {/* Info: (202501218 - Julian) Cover Image */}
+        <div className="relative h-180px w-full shrink-0">
+          <Image src={coverImageId} alt="Funding Cover" fill objectFit="cover" />
+
+          {/* Info: (202501218 - Julian) Token & Industry */}
+          {displayedCoverOverlay}
         </div>
-        {/* Info: (202501218 - Julian) Funding Progress */}
-        <ProgressBar
-          percentage={progressPercentage}
-          color={ProgressBarColor.GRADIENT}
-          size={ProgressBarSize.BASE}
-          minText={raisedFundingText}
-          maxText={goalFundingText}
-        />
-        {/* Info: (202501218 - Julian) Funding Stats */}
-        <div className="grid grid-cols-3 px-spacing-lv-4 pb-spacing-lv-4 pt-spacing-lv-2">
-          <div className="flex flex-col items-center">
-            <p className="text-lg font-bold text-text-neutral-primary">{leftStatData.value}</p>
-            <p className="text-xs font-normal text-text-neutral-tertiary">{leftStatData.label}</p>
+
+        {/* Info: (202501218 - Julian) Content */}
+        <div className="flex flex-col">
+          {/* Info: (202501218 - Julian) Company Name and Title */}
+          <div className="flex flex-col gap-spacing-lv-0 px-spacing-lv-6 py-spacing-lv-3">
+            <p className="text-xs font-normal text-text-neutral-tertiary">{companyName}</p>
+            <p
+              className={`${isLocked ? 'text-text-state-mute' : 'text-text-neutral-primary'} font-bold`}
+            >
+              {title}
+            </p>
           </div>
-          <div className="flex flex-col items-center border-x border-border-neutral-strong">
-            <p className="text-lg font-bold text-text-neutral-primary">{centerStatData.value}</p>
-            <p className="text-xs font-normal text-text-neutral-tertiary">{centerStatData.label}</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <p className="text-lg font-bold text-text-neutral-primary">{rightStatData.value}</p>
-            <p className="text-xs font-normal text-text-neutral-tertiary">{rightStatData.label}</p>
+          {/* Info: (202501218 - Julian) Funding Progress */}
+          <ProgressBar
+            percentage={progressPercentage}
+            color={ProgressBarColor.GRADIENT}
+            size={ProgressBarSize.BASE}
+            minText={raisedFundingText}
+            maxText={goalFundingText}
+            disabled={isLocked}
+          />
+          {/* Info: (202501218 - Julian) Funding Stats */}
+          <div className="grid grid-cols-3 px-spacing-lv-4 pb-spacing-lv-4 pt-spacing-lv-2">
+            <div className="flex flex-col items-center">
+              <p className={statValueStyle}>{leftStatData.value}</p>
+              <p className={statLabelStyle}>{leftStatData.label}</p>
+            </div>
+            <div className="flex flex-col items-center border-x border-border-neutral-strong">
+              <p className={statValueStyle}>{centerStatData.value}</p>
+              <p className={statLabelStyle}>{centerStatData.label}</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <p className={statValueStyle}>{rightStatData.value}</p>
+              <p className={statLabelStyle}>{rightStatData.label}</p>
+            </div>
           </div>
         </div>
       </div>

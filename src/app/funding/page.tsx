6afa8bc4aa7b88e4a81549a4e6734fb57a156182
julian <julian890701@gmail.com>
2025-12-19 15:04:31 +0800

    sidebar

--- src/app/funding/page.tsx
+++ src/app/funding/page.tsx
@@ -2,17 +2,16 @@
 
 import FundingTicket from '@/components/funding/funding_ticket';
 import { mockFundingItems } from '@/interfaces/funding';
+import Layout from '@/components/common/layout';
 
 export default function FundingPage() {
   return (
-    <main className="flex min-h-screen w-full flex-col overflow-x-hidden bg-surface-neutral-background">
-      <div></div>
-
+    <Layout>
       <div className="grid grid-cols-2 gap-spacing-lv-7 p-2">
         {mockFundingItems.map((item) => (
           <FundingTicket key={item.id} data={item} />
         ))}
       </div>
-    </main>
+    </Layout>
   );
 }
--- src/contexts/global_context.tsx
+++ src/contexts/global_context.tsx
@@ -3,11 +3,15 @@
 import React, { useState, useContext, createContext, useMemo } from 'react';
 
 interface IGlobalContextType {
+  isSidebarOpen: boolean;
+  sidebarToggleHandler: () => void;
   isReviewedTerms: boolean;
   reviewedTermsHandler: (isReviewed: boolean) => void;
 }
 
 const GlobalContext = createContext<IGlobalContextType>({
+  isSidebarOpen: true,
+  sidebarToggleHandler: () => {},
   isReviewedTerms: false,
   reviewedTermsHandler: () => {},
 });
@@ -17,17 +21,23 @@ interface IGlobalProviderProps {
 }
 
 export const GlobalProvider = ({ children }: IGlobalProviderProps) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true);
   const [isReviewedTerms, setIsReviewedTerms] = useState<boolean>(false);
+
+  // Info: (20251219 - Julian) Sidebar toggle handler
+  const sidebarToggleHandler = () => setIsSidebarOpen((prev) => !prev);
 
   // Info: (20251218 - Julian) Visibility handler for Modals
   const reviewedTermsHandler = (isReviewed: boolean) => setIsReviewedTerms(isReviewed);
 
   const value = useMemo(
     () => ({
+      isSidebarOpen,
+      sidebarToggleHandler,
       isReviewedTerms,
       reviewedTermsHandler,
     }),
-    [isReviewedTerms, reviewedTermsHandler]
+    [isSidebarOpen, sidebarToggleHandler, isReviewedTerms, reviewedTermsHandler]
   );
 
   return <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>;

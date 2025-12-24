a8e403d2f92ef375666f333112191b70dab79506
Tzuhan <crocodilestear@gmail.com>
2025-12-24 11:12:37 +0800

    feat: update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
-// eslint-disable-next-line no-restricted-imports
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
